4e9c309ad2a6201c5dc4fc5e7de927b37745bb7d
Belmy Maripane <bsmaripane@gmail.com>
2025-03-14 15:14:32 +0200

    14/03/2025 added initialsation and comparison to WorkingWithCppStrings file

--- Project_examples/WorkingWithCppStrings/WorkingWithCppStrings/main.cpp
+++ Project_examples/WorkingWithCppStrings/WorkingWithCppStrings/main.cpp
@@ -13,14 +13,37 @@ int main() {
 	string name4{ "apple" };
 	string name5{ name1 };
 	string name6{ name1, 0, 3 };
-	string name7{ 30,'X' };
+	string name7( 15,'X' );
 
 	cout << name0 << endl;		// No garbage
 	cout << name0.length() << endl;		// empty string
 
+	/************************************************************************************************************************/
 	// initialization
 	cout << endl << "Initialization" << endl << "-----------------------------------------------------------------" << endl;
+	cout << "Nmae0 is initialised to: " << name0 << endl;
+	cout << "Nmae1 is initialised to: " << name1 << endl;
+	cout << "Nmae2 is initialised to: " << name2 << endl;
+	cout << "Nmae3 is initialised to: " << name3 << endl;
+	cout << "Nmae4 is initialised to: " << name4 << endl;
+	cout << "Nmae5 is initialised to: " << name5 << endl;
+	cout << "Nmae6 is initialised to: " << name6 << endl;
+	cout << "Nmae7 is initialised to: " << name7 << endl;
 
+	/************************************************************************************************************************/
+	// Comparison
+	cout << endl << "Comparison" << endl << "-----------------------------------------------------------------" << endl;
+	cout << boolalpha;
+	cout << name1 << " == " << name5 << ": " << (name1 == name5) << endl;
+	cout << name1 << " == " << name2 << ": " << (name1 == name2) << endl;
+	cout << name1 << " != " << name2 << ": " << (name1 != name2) << endl;
+	cout << name1 << " < " << name2 << ": " << (name1 < name2) << endl;
+	cout << name2 << " > " << name1 << ": " << (name2 > name1) << endl;
+	cout << name4 << " < " << name5 << ": " << (name4 < name5) << endl;
+	cout << name1 << " == " << "Apple" << ": " << (name1 == "Apple") << endl;
+	
+	/***********************************************************************************************************************/
+	// Assignment
 
 
 	return 0;
