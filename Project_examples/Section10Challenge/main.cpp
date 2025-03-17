db8a3d2af16d0e6cfe013a11041bdf177020ee70
Belmy Maripane <bsmaripane@gmail.com>
2025-03-17 10:16:53 +0200

    17/03/2025 improved and better vision from ChatGPT

--- Project_examples/Section10Challenge/main.cpp
+++ Project_examples/Section10Challenge/main.cpp
@@ -23,117 +23,49 @@ To decrypt, replace the character at position n in key with the character at pos
 
 using namespace std;
 
+const string alphabet{ "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+const string key{ "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm" };
+
+string encrypt(const string& message) {
+    string encrypted_message;
+    for (char c : message) {
+        size_t index = alphabet.find(c);
+        if (index != string::npos) {
+            encrypted_message += key[index];
+        }
+        else {
+            encrypted_message += c; // Keep non-alphabet characters unchanged
+        }
+    }
+    return encrypted_message;
+}
+
+string decrypt(const string& message) {
+    string decrypted_message;
+    for (char c : message) {
+        size_t index = key.find(c);
+        if (index != string::npos) {
+            decrypted_message += alphabet[index];
+        }
+        else {
+            decrypted_message += c;
+        }
+    }
+    return decrypted_message;
+}
+
 int main() {
-	string secret_message;
-	string decrypted_secret_message;
-	string encrypted_secret_message;
+    string secret_message;
 
-	cout << "Enter a secret message: ";
-	getline(cin, secret_message);
+    cout << "Enter a secret message: ";
+    getline(cin, secret_message);
 
-	// Encryted the secret message
-	for (rsize_t i{}; i < secret_message.size(); ++i) {
-		if (secret_message.at(i) == 'a' || secret_message.at(i) == 'b' || secret_message.at(i) == 'c') {
-			if (secret_message.at(i) == 'a')
-				encrypted_secret_message.insert(i, "2");
-			else if (secret_message.at(i) == 'b')
-				encrypted_secret_message.insert(i, "2'");
-			else if (secret_message.at(i) == 'c')
-				encrypted_secret_message.insert(i, "2''");
-		}
-		else if (secret_message.at(i) == 'g' || secret_message.at(i) == 'h' || secret_message.at(i) == 'i') {
-			if (secret_message.at(i) == 'g')
-				encrypted_secret_message.insert(i, "4");
-			else if (secret_message.at(i) == 'h')
-				encrypted_secret_message.insert(i, "4'");
-			else if (secret_message.at(i) == 'i')
-				encrypted_secret_message.insert(i, "4''");
-		}
-		else if (secret_message.at(i) == 'm' || secret_message.at(i) == 'n' || secret_message.at(i) == 'o') {
-			if (secret_message.at(i) == 'm')
-				encrypted_secret_message.insert(i, "6");
-			else if (secret_message.at(i) == 'n')
-				encrypted_secret_message.insert(i, "6'");
-			else if (secret_message.at(i) == 'o')
-				encrypted_secret_message.insert(i, "6''");
-		}
-		else if (secret_message.at(i) == 't' || secret_message.at(i) == 'u' || secret_message.at(i) == 'v') {
-			if (secret_message.at(i) == 't')
-				encrypted_secret_message.insert(i, "8");
-			else if (secret_message.at(i) == 'u')
-				encrypted_secret_message.insert(i, "8'");
-			else if (secret_message.at(i) == 'v')
-				encrypted_secret_message.insert(i, "8''");
-		}
-		else if (secret_message.at(i) == 'D' || secret_message.at(i) == 'J' || secret_message.at(i) == 'P' || secret_message.at(i) == 'W') {
-			if (secret_message.at(i) == 'D')
-				encrypted_secret_message.insert(i, "3");
-			else if (secret_message.at(i) == 'J')
-				encrypted_secret_message.insert(i, "5");
-			else if (secret_message.at(i) == 'P')
-				encrypted_secret_message.insert(i, "7");
-			else if (secret_message.at(i) == 'W')
-				encrypted_secret_message.insert(i, "9");
-		}
-		else
-			encrypted_secret_message.push_back(secret_message.at(i));
-	}
+    string encrypted_message = encrypt(secret_message);
+    string decrypted_message = decrypt(encrypted_message);
 
-	// Decrypted the secret message
-	for (size_t i{}; i < secret_message.size() ; i++)
-	{
-		if (encrypted_secret_message.at(i) == '2') {
-			if (encrypted_secret_message.at(i) == '2' && encrypted_secret_message.at(i + 1) != '\'')
-				decrypted_secret_message.insert(i, "a");
-			else if (encrypted_secret_message.at(i) == '2' && encrypted_secret_message.at(i + 1) == '\'')
-				decrypted_secret_message.insert(i, "b");
-			else if (encrypted_secret_message.at(i) == '2' && encrypted_secret_message.at(i + 1) == '\'' && encrypted_secret_message.at(i + 2) == '\'')
-				decrypted_secret_message.insert(i, "c");
-		}
-		else if (encrypted_secret_message.at(i) == '4') {
-			if (encrypted_secret_message.at(i) == '4' && encrypted_secret_message.at(i + 1) != '\'')
-				decrypted_secret_message.insert(i, "g");
-			else if (encrypted_secret_message.at(i) == '4' && encrypted_secret_message.at(i + 1) == '\'')
-				decrypted_secret_message.insert(i, "h");
-			else if (encrypted_secret_message.at(i) == '4' && encrypted_secret_message.at(i + 1) == '\'' && encrypted_secret_message.at(i + 2) == '\'')
-				decrypted_secret_message.insert(i, "i");
-		}
-		else if (encrypted_secret_message.at(i) == '6') {
-			if (encrypted_secret_message.at(i) == '6' && encrypted_secret_message.at(i + 1) != '\'')
-				decrypted_secret_message.insert(i, "mg");
-			else if (encrypted_secret_message.at(i) == '6' && encrypted_secret_message.at(i + 1) == '\'')
-				decrypted_secret_message.insert(i, "n");
-			else if (encrypted_secret_message.at(i) == '6' && encrypted_secret_message.at(i + 1) == '\'' && encrypted_secret_message.at(i + 2) == '\'')
-				decrypted_secret_message.insert(i, "o");
-		}
-		else if (encrypted_secret_message.at(i) == '8') {
-			if (encrypted_secret_message.at(i) == '8' && encrypted_secret_message.at(i + 1) != '\'')
-				decrypted_secret_message.insert(i, "t");
-			else if (encrypted_secret_message.at(i) == '8' && encrypted_secret_message.at(i + 1) == '\'')
-				decrypted_secret_message.insert(i, "u");
-			else if (encrypted_secret_message.at(i) == '8' && encrypted_secret_message.at(i + 1) == '\'' && encrypted_secret_message.at(i + 2) == '\'')
-				decrypted_secret_message.insert(i, "v");
-		}
-		else if (encrypted_secret_message.at(i) == '3' || encrypted_secret_message.at(i) == '5' || encrypted_secret_message.at(i) == '7' || encrypted_secret_message.at(i) == '9') {
-			if (encrypted_secret_message.at(i) == '3')
-				decrypted_secret_message.insert(i, "D");
-			else if (encrypted_secret_message.at(i) == '5')
-				decrypted_secret_message.insert(i, "J");
-			else if (encrypted_secret_message.at(i) == '7')
-				decrypted_secret_message.insert(i, "P");
-			else if (encrypted_secret_message.at(i) == '9')
-				decrypted_secret_message.insert(i, "W");
-		}
-		else
-			if (encrypted_secret_message.at(i) != '\'')
-				decrypted_secret_message.push_back(encrypted_secret_message.at(i));
-	}
+    cout << "\nSecret message: " << secret_message;
+    cout << "\nEncrypted message: " << encrypted_message;
+    cout << "\nDecrypted message: " << decrypted_message << endl;
 
-
-	// Display the secret, encryped and decrypted message
-	cout << "\nSecret message:\n" << secret_message
-		<< "\n\nEncrypted message:\n" << encrypted_secret_message
-		<< "\n\nDecrypted message:\n" << decrypted_secret_message << endl;
-
-	return 0;
+    return 0;
 }
