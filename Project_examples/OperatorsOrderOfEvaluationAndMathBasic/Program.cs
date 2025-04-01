c8391dec541281a84d18afa35b227cd3e4318024
Belmy Maripane <bsmaripane@gmail.com>
2025-04-01 05:27:13 +0200

    01/04/2025 modulation

--- Project_examples/OperatorsOrderOfEvaluationAndMathBasic/Program.cs
+++ Project_examples/OperatorsOrderOfEvaluationAndMathBasic/Program.cs
@@ -13,13 +13,15 @@ namespace OperatorsOrderOfEvaluationAndMathBasic
             // Concatination
             Console.WriteLine("Age is " + number1);
             Console.WriteLine("Addition number1 + number2 = " + (number1 + number2));
+            Console.WriteLine($"Addition number1 + number2 {number1 + number2}");
 
             // Order of Evaluation
             Console.WriteLine("Subtraction number1 - number2 = " + (number1 - number2));
             Console.WriteLine("Multiplication number1 * number2 = " + number1 * number2);
             Console.WriteLine("Division number1 / number2 = " + number1 / number2);
+            Console.WriteLine($"Modulation number1 % number2 {number1 % number2}");
 
-            // 
+            // basic math
 
             // Wait for the key press before closing
             Console.ReadKey();
