77a490ca6a5233deab755de84db0fe573ddec600
Belmy Maripane <bsmaripane@gmail.com>
2025-04-01 05:20:58 +0200

    01/04/2025 Operators and order of evaluation

---
+++ Project_examples/OperatorsOrderOfEvaluationAndMathBasic/Program.cs
@@ -0,0 +1,28 @@
+﻿// Operators and Order of Evaluation
+
+namespace OperatorsOrderOfEvaluationAndMathBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Decaring and initialising variables
+            int number1 = 5;
+            int number2 = 13;
+
+            // Concatination
+            Console.WriteLine("Age is " + number1);
+            Console.WriteLine("Addition number1 + number2 = " + number1 + number2);
+
+            // Order of Evaluation
+            Console.WriteLine("Subtraction number1 - number2 = " + number1 - number2);
+            Console.WriteLine("Multiplication number1 * number2 = " + number1 * number2);
+            Console.WriteLine("Division number1 / number2 = " + number1 / number2);
+
+            // 
+
+            // Wait for the key press before closing
+            Console.ReadKey();
+        }
+    }
+}
