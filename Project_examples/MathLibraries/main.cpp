99f77c5ac9eebc07167fb1f66fcfd874a4f9d5c9
Belmy Maripane <bsmaripane@gmail.com>
2025-04-08 17:34:21 +0200

    08/04/2025 update the code based on the lesson

--- Project_examples/MathLibraries/main.cpp
+++ Project_examples/MathLibraries/main.cpp
@@ -11,13 +11,7 @@ int main()
     double number{};
 
     cout << "Enter a number (double): ";
-    while (!(cin >> number)) {
-        // Clear the error flag on cin
-        cin.clear();
-        // Ignore the invalid input
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Invalid input. Please enter a valid number: ";
-    }
+    cin >> number;
     
     cout << "The sqrt of " << number << " is " << sqrt(number) << endl;
     cout << "The cubed root of " << number << " is " << cbrt(number) << endl;
@@ -26,12 +20,16 @@ int main()
     cout << "The cosine of " << number << " is " << cos(number) << endl;
 
     cout << "The ceil of " << number << " is " << ceil(number) << endl;
+    cout << "The floor of " << number << " is " << floor(number) << endl;
+    cout << "The round of " << number << " is " << round(number) << endl;
 
-    // Wait for user input before closing the console
-    cout << "Press Enter to exit...";
-    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore any remaining input
-    cin.get(); // Wait for the user to press Enter
+    double power{};
 
+    cout << "\nEnter a power to raise " << number << " to: ";
+    cin >> power;
+
+    cout << number << " raised to the " << power << " power is " << pow(number, power) << endl;
+   
     return 0;
 }
 
