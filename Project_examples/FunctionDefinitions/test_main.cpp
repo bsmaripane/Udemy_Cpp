34dfda8e0d8a3f10dcd890b445a1c57644a15068
Belmy Maripane <bsmaripane@gmail.com>
2025-04-14 17:46:16 +0200

    14/04/2025 commented the test files

--- Project_examples/FunctionDefinitions/test_main.cpp
+++ Project_examples/FunctionDefinitions/test_main.cpp
@@ -1,23 +1,23 @@
-#include <gtest/gtest.h>
-#include "math_utils.h" // Include the header file with function declarations
-
-// Test for calc_area_circle
-TEST(CircleTests, PositiveRadius) {
-    EXPECT_NEAR(calc_area_circle(1.0), 3.14159, 0.00001);
-    EXPECT_NEAR(calc_area_circle(2.0), 12.56636, 0.00001);
-}
-
-TEST(CircleTests, ZeroRadius) {
-    EXPECT_NEAR(calc_area_circle(0.0), 0.0, 0.00001);
-}
-
-// Test for calc_volume_cylinder
-TEST(CylinderTests, PositiveDimensions) {
-    EXPECT_NEAR(calc_volume_cylinder(1.0, 1.0), 3.14159, 0.00001);
-    EXPECT_NEAR(calc_volume_cylinder(2.0, 3.0), 37.69908, 0.00001);
-}
-
-TEST(CylinderTests, ZeroDimensions) {
-    EXPECT_NEAR(calc_volume_cylinder(0.0, 1.0), 0.0, 0.00001);
-    EXPECT_NEAR(calc_volume_cylinder(1.0, 0.0), 0.0, 0.00001);
-}
+//#include <gtest/gtest.h>
+//#include "math_utils.h" // Include the header file with function declarations
+//
+//// Test for calc_area_circle
+//TEST(CircleTests, PositiveRadius) {
+//    EXPECT_NEAR(calc_area_circle(1.0), 3.14159, 0.00001);
+//    EXPECT_NEAR(calc_area_circle(2.0), 12.56636, 0.00001);
+//}
+//
+//TEST(CircleTests, ZeroRadius) {
+//    EXPECT_NEAR(calc_area_circle(0.0), 0.0, 0.00001);
+//}
+//
+//// Test for calc_volume_cylinder
+//TEST(CylinderTests, PositiveDimensions) {
+//    EXPECT_NEAR(calc_volume_cylinder(1.0, 1.0), 3.14159, 0.00001);
+//    EXPECT_NEAR(calc_volume_cylinder(2.0, 3.0), 37.69908, 0.00001);
+//}
+//
+//TEST(CylinderTests, ZeroDimensions) {
+//    EXPECT_NEAR(calc_volume_cylinder(0.0, 1.0), 0.0, 0.00001);
+//    EXPECT_NEAR(calc_volume_cylinder(1.0, 0.0), 0.0, 0.00001);
+//}
