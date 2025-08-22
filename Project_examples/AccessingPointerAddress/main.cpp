ab0f945751a072a4853be4777992fb75f8bb9acd
Belmy Maripane <bsmaripane@gmail.com>
2025-08-22 16:09:13 +0200

    Understanding the pointer

--- Project_examples/AccessingPointerAddress/main.cpp
+++ Project_examples/AccessingPointerAddress/main.cpp
@@ -14,13 +14,38 @@ int main()
 	cout << "sizeof of num is: " << sizeof(num) << " bytes" << endl;
 	cout << "Address of num is: " << &num << endl;
 
-	int* p;
+	//int* p;
 
-	cout << "Value of p is: " << p << endl;	// garbage
-	cout << "Address p is: " << &p << endl;
-	cout << "sizeof of p is: " << sizeof(p) << " bytes" << endl;
-	p = nullptr;	// set p to point nowhere
-	cout << "Value of p is: " << p << endl;
+	//cout << "Value of p is: " << p << endl;	// garbage
+	//cout << "Address p is: " << &p << endl;
+	//cout << "sizeof of p is: " << sizeof(p) << " bytes" << endl;
+	//p = nullptr;	// set p to point nowhere
+	//cout << "Value of p is: " << p << endl;
+
+	int* p1{ nullptr };
+	double* p2{ nullptr };
+	unsigned long long* p3{ nullptr };
+	vector<string>* p4{ nullptr };
+	string* p5{ nullptr };
+
+	cout << endl << "sizeof p1: " << sizeof(p1) << " bytes" << endl;
+	cout << "sizeof p2: " << sizeof(p2) << " bytes" << endl;
+	cout << "sizeof p3: " << sizeof(p3) << " bytes" << endl;
+	cout << "sizeof p4: " << sizeof(p4) << " bytes" << endl;
+	cout << "sizeof p5: " << sizeof(p5) << " bytes" << endl;
+
+
+
+	int score{ 10 };
+	double high_temp{ 100.7 };
+
+	int* score_ptr{ nullptr };
+
+	score_ptr = &score;
+
+	cout << "Value of score is: " << score << endl;
+	cout << "Address of score is: " << &score << endl;
+	cout << "Value of score_ptr is: " << score_ptr << endl;
 
 	return 0;
 }
