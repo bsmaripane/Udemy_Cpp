a6684b43b2a08dbd416132f14c36adeb0d273bdf
Belmy Maripane <bsmaripane@gmail.com>
2025-08-25 15:32:28 +0200

    Another derenferencing a pointer example

--- Project_examples/DereferencingPointer/main.cpp
+++ Project_examples/DereferencingPointer/main.cpp
@@ -17,5 +17,24 @@ int main()
 	cout << "*score_ptr is: " << *score_ptr << endl;
 	cout << "score is: " << score << endl;
 
+	// Another example of double
+	double high_temp{ 100.7 };
+	double low_temp{ 37.4 };
+	double *temp_ptr{&high_temp};
+
+	cout << "\n*temp_ptr{&high_temp}  is: " << *temp_ptr << endl;
+
+	temp_ptr = &low_temp;
+
+	cout << "temp_ptr = &low_temp is: " << *temp_ptr << endl;
+
+	// Another example of string
+	string name{ "Belmy" };
+	string* string_ptr{ &name };
+
+	cout << "\n*string_ptr{ &name } is: " << *string_ptr << endl;
+	name = "James";
+	cout << "*string_ptr after name changed to james is: " << *string_ptr << endl;
+
 	return 0;
 }
