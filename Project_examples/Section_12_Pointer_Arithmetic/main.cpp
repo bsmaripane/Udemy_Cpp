2f12e70984653517c618741863a2369f4af19459
Belmy Maripane <bsmaripane@gmail.com>
2025-08-28 17:30:20 +0200

    Pointer comparision arithmetic

--- Project_examples/Section_12_Pointer_Arithmetic/main.cpp
+++ Project_examples/Section_12_Pointer_Arithmetic/main.cpp
@@ -16,9 +16,30 @@ int main()
 	}
 
 	cout << "\n--------------------------------------------\n";
+
 	score_ptr = scores;
 
 	while (*score_ptr != -1) cout << *score_ptr++ << endl;
+
+	cout << "\n--------------------------------------------\n";
+
+	string s1{ "Belmy" };
+	string s2{ "Belmy" };
+	string s3{ "Felicia" };
+
+	string* p1{ &s1 };
+	string* p2{ &s2 };
+	string* p3{ &s1 };
+
+	cout << boolalpha;
+	cout << p1 << " == " << p2 << ": " << (p1 == p2) << endl;
+	cout << p1 << " == " << p3 << ": " << (p1 == p3) << endl;
+
+	cout << *p1 << " == " << *p2 << ": " << (*p1 == *p2) << endl;
+	cout << *p1 << " == " << *p3 << ": " << (*p1 == *p3) << endl;
+
+	p3 = &s3;
+	cout << *p1 << " == " << *p3 << ": " << (*p1 == *p3) << endl;
 
 	cout << "\n--------------------------------------------\n";
 
