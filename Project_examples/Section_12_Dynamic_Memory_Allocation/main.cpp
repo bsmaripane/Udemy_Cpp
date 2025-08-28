ecba120cbe14f1fd0899e8744ae0239ef322d2a8
Belmy Maripane <bsmaripane@gmail.com>
2025-08-28 14:31:05 +0200

    using delete keyword to deallocate storage

--- Project_examples/Section_12_Dynamic_Memory_Allocation/main.cpp
+++ Project_examples/Section_12_Dynamic_Memory_Allocation/main.cpp
@@ -9,7 +9,7 @@ int main()
 {
 	int* int_ptr{ nullptr };
 
-	int_ptr = new int;
+	int_ptr = new int;	// allocate an integer on the heap
 
 	cout << "int_ptr is: " << int_ptr << endl;
 	cout << "*int_ptr is: " << *int_ptr << endl;
@@ -17,6 +17,10 @@ int main()
 	*int_ptr = 100;
 
 	cout << "*int_ptr after assigning 100 is " << *int_ptr << endl;
+
+	// using delete to deallocate storage
+
+	delete int_ptr;		// frees the allocated storage
 
 	return 0;
 }
