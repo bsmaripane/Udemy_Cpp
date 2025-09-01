3a93aae5cc7acd067ed2173c8c1212f5e1da6851
Belmy Maripane <bsmaripane@gmail.com>
2025-09-01 14:04:10 +0200

    ERROR: 'score_ptr': you  cannot assign to a variable that is const

---
+++ Project_examples/Section_12_ConstantPointers/main.cpp
@@ -0,0 +1,22 @@
+// Constant pointers
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int high_score{ 100 };
+	int low_score{ 65 };
+	int* const score_ptr{ &high_score };
+
+	cout << "*score_ptr is " << *score_ptr << endl;
+
+	*score_ptr = 86;
+
+	cout << "*score_ptr is " << *score_ptr << endl;
+
+	score_ptr = &low_score;		// ERROR: 'score_ptr': you  cannot assign to a variable that is const
+
+	return 0;
+}
