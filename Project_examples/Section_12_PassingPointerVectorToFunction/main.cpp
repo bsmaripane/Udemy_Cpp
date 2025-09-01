89a3a4531f291afa81d5dd8e54e6d04b08bf9a68
Belmy Maripane <bsmaripane@gmail.com>
2025-09-01 15:16:19 +0200

    Passing pointer vector to a function

---
+++ Project_examples/Section_12_PassingPointerVectorToFunction/main.cpp
@@ -0,0 +1,35 @@
+// Section 12 Passing pointer vector to a function
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void display(vector<string>* v)
+{
+	for (auto str : *v)
+		cout << str << " ";
+	cout << endl;
+}
+
+void display(int* array, int sentinel)
+{
+	for (int i{}; i != sentinel; ++i)
+		cout << *(array + i) << endl;
+
+	cout << endl;
+}
+
+int main()
+{
+	cout << "\n__________________________________\n";
+	vector<string> names{ "Belmy", "Semape", "Maswi", "Thapelo" };
+	display(&names);
+
+	cout << "\n__________________________________\n";
+	int scores[]{ 100, 98, 97, 79, 85,-1 };
+	display(scores, -1);
+
+	return 0;
+}
