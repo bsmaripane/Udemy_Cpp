de000fcc8cad9fa064b4278c304890cc41462ac7
Belmy Maripane <bsmaripane@gmail.com>
2025-09-01 15:55:08 +0200

    Returing dynamically allocated memory

---
+++ Project_examples/Section_12_ReturningDynamicallyAllocatedMemory/main.cpp
@@ -0,0 +1,33 @@
+// Returing dynamically allocated memory
+
+#include <iostream>
+
+using namespace std;
+
+int* create_array(size_t size, int init_value = 0)
+{
+	int* new_storage{ nullptr };
+
+	new_storage = new int[size];
+
+	for (rsize_t i{ 0 }; i < size; ++i)
+		*(new_storage + i) = init_value;
+
+	return new_storage;
+}
+
+
+int main()
+{
+	int* my_array;		// will be allocated by the function
+
+	my_array = create_array(100, 20);	// create the array
+
+	for (size_t i{}; i < 100; ++i)
+		cout << *(my_array + i) << " ";
+	cout << endl;
+
+	delete[] my_array;	// free the storage
+
+	return 0;
+}
