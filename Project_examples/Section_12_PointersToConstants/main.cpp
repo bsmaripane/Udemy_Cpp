5a655f344d2aa9172f968f86013c4a873ad08109
Belmy Maripane <bsmaripane@gmail.com>
2025-09-01 13:50:44 +0200

    Pointers to constants

---
+++ Project_examples/Section_12_PointersToConstants/main.cpp
@@ -0,0 +1,23 @@
+//
+// Pointers to constants
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int high_score{ 100 };
+	int low_score{ 65 };
+	const int* score_ptr{ &high_score };
+
+	cout << "*score_ptr is " << *score_ptr << endl;
+
+	//*score_ptr = 86;
+
+	score_ptr = &low_score;
+	
+	cout << "*score_ptr is " << *score_ptr << endl;
+
+	return 0;
+}
