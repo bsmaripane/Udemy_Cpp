f4d9d445009c193e917a79a7d9f3fd47fe470c60
Belmy Maripane <bsmaripane@gmail.com>
2025-11-26 17:46:57 +0200

    A structure of a class

---
+++ Project_examples/DeclareClassesAndObjects/DeclareClassesAndObjects/DeclareClassesAndObjects.cpp
@@ -0,0 +1,21 @@
+// Section 13: Declaring Classes and Objects
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Player 
+{
+    // Attributes
+
+    // Methods
+};
+
+int main()
+{
+    
+    cin.get();
+    return 0;
+}
