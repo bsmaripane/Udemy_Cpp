9d2398dfba6f52033d75c33442ac486f6cd7a01e
Belmy Maripane <bsmaripane@gmail.com>
2025-11-27 07:48:51 +0200

    Creating the objects of the class and pointer object

--- Project_examples/DeclareClassesAndObjects/DeclareClassesAndObjects/DeclareClassesAndObjects.cpp
+++ Project_examples/DeclareClassesAndObjects/DeclareClassesAndObjects/DeclareClassesAndObjects.cpp
@@ -9,12 +9,24 @@ using namespace std;
 class Player 
 {
     // Attributes
+    string name;
+    int health;
+    int xp;
 
     // Methods
+    void talk(string);
+    bool is_dead();
 };
 
 int main()
 {
+    Player semape;
+    Player master;
+
+    Player* enemy{ nullptr };
+    enemy = new Player;
+
+    delete enemy;
     
     cin.get();
     return 0;
