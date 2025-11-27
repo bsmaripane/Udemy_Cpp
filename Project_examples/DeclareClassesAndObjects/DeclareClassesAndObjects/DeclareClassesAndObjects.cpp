35e783289362c4b47b82bb91e7d11ea4abf5e92c
Belmy Maripane <bsmaripane@gmail.com>
2025-11-27 09:58:42 +0200

    define the functions

--- Project_examples/DeclareClassesAndObjects/DeclareClassesAndObjects/DeclareClassesAndObjects.cpp
+++ Project_examples/DeclareClassesAndObjects/DeclareClassesAndObjects/DeclareClassesAndObjects.cpp
@@ -8,14 +8,28 @@ using namespace std;
 
 class Player 
 {
+
     // Attributes
+private:
     string name;
     int health;
     int xp;
 
     // Methods
-    void talk(string);
-    bool is_dead();
+public:
+    Player() : name (""), health (100), xp (0) {}
+
+    void talk(string strWords)
+    {
+        cout << name << ": " << strWords << endl;
+    }
+
+    bool is_dead()
+    {
+        if (health > 0)
+            return false;
+        return true;
+    }
 };
 
 int main()
@@ -23,7 +37,7 @@ int main()
     Player semape;
     Player master;
 
-    Player players[]{ semape, master };
+    Player players[]{ semape, master};
 
     Player* enemy{ nullptr };
     enemy = new Player;
