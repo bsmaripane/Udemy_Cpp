c4ab9a766ae854472f758e865445369e783414bf
Belmy Maripane <bsmaripane@gmail.com>
2025-11-29 19:36:26 +0200

    Declare and define the constructor methods of the Account class.

--- Project_examples/PlayerConstructor/main.cpp
+++ Project_examples/PlayerConstructor/main.cpp
@@ -14,13 +14,25 @@ public:
 	Player();
 	Player(std::string pName);
 	Player(std::string pName, int pHealth, int pXp);
+};
 
+class Account
+{
+private:
+	std::string name;
+	double balance;
+
+public:
+	Account();
+	Account(std::string aName);
+	Account(std::string aName, double aBalance);
+	Account(double aBalance);
 };
 
 int main()
 {
 	Player thapelo, lesego("Spider-Man"), kganya("Black Panther", 93, 67);
-
+	Account semape(1784.57), thato, maswi("Maswi's account"), kg("Kganya's account", 235.89);
 
 	std::cin.get();
 	return 0;
@@ -29,3 +41,8 @@ int main()
 Player::Player() : name("None"), health(10), xp(0) {}
 Player::Player(std::string pName) : name(pName), health(10), xp(0) {}
 Player::Player(std::string pName, int pHealth, int pXp) : name(pName), health(pHealth), xp(pXp) {}
+
+Account::Account() : name(""), balance(0.0) {}
+Account::Account(std::string aName) : name(aName), balance(0.0) {}
+Account::Account(std::string aName, double aBalance) : name(aName), balance(aBalance) {}
+Account::Account(double aBalance) : name("noname"), balance(aBalance) {}
