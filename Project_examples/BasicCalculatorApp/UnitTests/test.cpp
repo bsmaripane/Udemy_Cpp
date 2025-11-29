61df45be371bbf9db4b8e33ae4419976868ffb6b
Belmy Maripane <bsmaripane@gmail.com>
2025-11-29 18:09:28 +0200

    Completed the tests

--- Project_examples/BasicCalculatorApp/UnitTests/test.cpp
+++ Project_examples/BasicCalculatorApp/UnitTests/test.cpp
@@ -1,9 +1,37 @@
 #include "pch.h"
 #include "gtest/gtest.h"
 #include "../BasicCalculatorApp/Calculator.h"
+#include "../BasicCalculatorApp/Calculator.cpp"
 
-TEST(Calculator, AddTwoNumbers)
+namespace CalculatorAppTest
 {
-	Calculator<int> calcAdd;
-	EXPECT_EQ(calcAdd.add(2,3), 5);
+	TEST(Calculator, AddTwoNumbers)
+	{
+		Calculator calcAdd;
+		EXPECT_EQ(calcAdd.add(2, 3), 5);
+	}
+
+	TEST(Calculator, MinusTwoNumbers)
+	{
+		Calculator calc;
+		EXPECT_EQ(calc.minus(2, 3), -1);
+	}
+
+	TEST(Calculator, MultipleTwoNumbers)
+	{
+		Calculator calc;
+		EXPECT_EQ(calc.multiple(2, 3), 6);
+	}
+
+	TEST(Calculator, DivideTwoNumbers)
+	{
+		Calculator calc;
+		EXPECT_EQ(calc.divide(13, 3), 4);
+	}
+
+	TEST(Calculator, ModuleTwoNumbers)
+	{
+		Calculator calc;
+		EXPECT_EQ(calc.module(9, 2), 1);
+	}
 }
