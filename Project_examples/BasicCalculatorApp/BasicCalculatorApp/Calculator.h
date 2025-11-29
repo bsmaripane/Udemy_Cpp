526cb3d3df37d9a6c67a15219d5de9059b1b0727
Belmy Maripane <bsmaripane@gmail.com>
2025-11-29 17:26:54 +0200

    Removed .cpp and add the definition of the class in .h sice it uses template format

--- Project_examples/BasicCalculatorApp/BasicCalculatorApp/Calculator.h
+++ Project_examples/BasicCalculatorApp/BasicCalculatorApp/Calculator.h
@@ -7,10 +7,27 @@ template <class Type>
 class Calculator
 {
 public:
-	Type add(Type x, Type y) const;
-	Type minus(Type x, Type y) const;
-	Type multiple(Type x, Type y) const;
-	Type divide(Type x, Type y) const;
-	Type module(Type x, Type y) const;
+	Type add(Type x, Type y) const { return (x + y); }
+	Type minus(Type x, Type y) const { return (x - y); }
+	Type multiple(Type x, Type y) const { return (x * y); }
+	Type divide(Type x, Type y) const
+	{
+		if (y == 0)
+		{
+			std::cerr << "Error: Division by zero is NOT allowed!" << std::endl;
+		}
+		else
+			return (x / y);
+	}
+
+	Type module(Type x, Type y) const
+	{
+		if (y == 0)
+		{
+			std::cerr << "Error: Division by zero is NOT allowed!" << std::endl;
+		}
+		else
+			return (x % y);
+	}
 };
 #endif
