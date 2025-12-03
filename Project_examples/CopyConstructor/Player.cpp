a15b52729cf54f505f38715812261cf0d192e2c1
Belmy Maripane <bsmaripane@gmail.com>
2025-12-03 19:52:42 +0200

    Definition of the method of the Player class

---
+++ Project_examples/CopyConstructor/Player.cpp
@@ -0,0 +1,14 @@
+#include "Player.h"
+
+Player::Player() : name(""), health(1), xp(0) {}
+Player::Player(std::string pName, int pHealth, int pXp) : name(pName), health(pHealth), xp(pXp) {}
+
+void Player::setName(std::string pName) { name = pName; }
+void Player::setHealth(int pHealth) { health = pHealth; }
+void Player::setHealth(int pXp) { xp = pXp; }
+
+std::string Player::getName() const { return name; }
+int Player::getHealth() const { return health; }
+int Player::getHealth() const { return xp; }
+
+Player::~Player() { std::cout << "\nDeallocating the memory!\n"; }
