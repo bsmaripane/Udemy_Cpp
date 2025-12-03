98d480237de34287accc8e0a082118002d8c96fe
Belmy Maripane <bsmaripane@gmail.com>
2025-12-03 20:09:21 +0200

    Implementing the copy constructor

--- Project_examples/CopyConstructor/Player.cpp
+++ Project_examples/CopyConstructor/Player.cpp
@@ -1,6 +1,7 @@
 #include "Player.h"
 
 Player::Player() : name(""), health(1), xp(0) {}
+Player::Player(std::string pName) : name(pName), health(1), xp(0) {}
 Player::Player(std::string pName, int pHealth, int pXp) : name(pName), health(pHealth), xp(pXp) {}
 
 void Player::setName(std::string pName) { name = pName; }
@@ -10,5 +11,7 @@ void Player::setHealth(int pXp) { xp = pXp; }
 std::string Player::getName() const { return name; }
 int Player::getHealth() const { return health; }
 int Player::getHealth() const { return xp; }
+
+Player::Player(const Player &source) : name(source.name), health(source.health), xp(source.xp) {}
 
 Player::~Player() { std::cout << "\nDeallocating the memory!\n"; }
