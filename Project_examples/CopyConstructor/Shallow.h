5c27b55bd8c10a9c09b85094a0dd4411ffcb0319
Belmy Maripane <bsmaripane@gmail.com>
2025-12-04 18:53:36 +0200

    Definition of the class Shallow to demonstrates the problem with a shallow copy

---
+++ Project_examples/CopyConstructor/Shallow.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef SHALLOW_H
+#define SHALLOW_H
+
+#include <iostream>
+
+class Shallow
+{
+private:
+	int* data;
+
+public:
+	Shallow(int d);
+	Shallow(const Shallow& source);
+	~Shallow();
+};
+#endif
