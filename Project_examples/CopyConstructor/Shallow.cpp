96026d42f46654211da9927f4ad25b5e5cf2af03
Belmy Maripane <bsmaripane@gmail.com>
2025-12-04 19:32:04 +0200

    Definition of the setDataValue, getDataValue and getDataAddress methods

--- Project_examples/CopyConstructor/Shallow.cpp
+++ Project_examples/CopyConstructor/Shallow.cpp
@@ -13,3 +13,7 @@ Shallow::~Shallow()
 }
 
 Shallow::Shallow(const Shallow& source) : data(source.data) { std::cout << "Copy constructor - shallow" << std::endl; }
+
+void Shallow::setDataValue(int d) { *data = d; }
+int Shallow::getDataValue() const { return *data; }
+int* Shallow::getDataAdddress() const { return data; }
