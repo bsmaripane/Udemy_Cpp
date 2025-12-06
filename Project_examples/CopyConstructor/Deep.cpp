628c2f7605d75f770ab229cc99338d2aac47055f
Belmy Maripane <bsmaripane@gmail.com>
2025-12-06 17:56:32 +0200

    Add destructor and deep copy constructor to Deep class
    A destructor was added to the `Deep` class to ensure proper
    deallocation of dynamically allocated memory (`data`) and to
    log a message when the destructor is called.
    
    A deep copy constructor was implemented to allocate new memory
    for the `data` member and copy the value from the source object,
    ensuring safe copying of `Deep` objects. A log message was added
    to indicate when the deep copy constructor is invoked.

--- Project_examples/CopyConstructor/Deep.cpp
+++ Project_examples/CopyConstructor/Deep.cpp
@@ -5,3 +5,16 @@ Deep::Deep(int d)
 	data = new int;		// allocate storage
 	*data = d;
 }
+
+Deep::~Deep()
+{
+	delete data;		// free storage
+	std::cout << "Destructor freeing data" << std::endl;
+}
+
+Deep::Deep(const Deep& source)
+{
+	data = new int;		// allocate storage
+	*data = *source.data;
+	std::cout << "Copy constructor - deep" << std::endl;
+}
