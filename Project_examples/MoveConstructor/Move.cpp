d74c5ecad2ae848b5393696d8b6732a14fde4948
Belmy Maripane <bsmaripane@gmail.com>
2025-12-06 20:35:48 +0200

    Update the display of the destructor method

--- Project_examples/MoveConstructor/Move.cpp
+++ Project_examples/MoveConstructor/Move.cpp
@@ -5,5 +5,12 @@ int Move::getDataValue() const { return *data; }
 int* Move::getDataAddress() const { return data; }
 Move::Move(int d) { data = new int; *data = d; std::cout << "\nConstructor for: " << d << std::endl; }
 Move::Move(const Move& source) : Move(*source.data) { std::cout << std::endl << "Copy constructor - deep copy for: " << *data << std::endl; }
-//Move::Move(Move&& source) : data(source.data) { source.data = nullptr; std::cout << "\n'Steal' the data and then null out the soucre pointer" << std::endl; }
-Move::~Move() { delete data; std::cout << "\nDeallocate the data memory for " << *data << std::endl; }
+//Move::Move(Move&& source) : data(source.data) { source.data = nullptr; std::cout << "\nMove constructor - moving resource: " << *data << std::endl; }
+Move::~Move()
+{ 
+	if (data != nullptr)
+		std::cout << "\nDestructor freeing data for: " << *data << std::endl;
+	else
+		std::cout << "\nDestructor freeing data for nullptr" << std::endl;
+	delete data;
+}
