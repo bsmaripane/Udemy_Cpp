96f9e03b8b1589fca61a01083f1073a144a4d0ec
Belmy Maripane <bsmaripane@gmail.com>
2025-12-06 20:49:35 +0200

    ### Commit Message
    Restore move constructor and add demonstration program
    
    The move constructor in `Move.cpp` was uncommented, restoring its implementation to transfer ownership of dynamically allocated resources safely. A new `main.cpp` file was added to demonstrate the functionality of the `Move` class, including the use of a `std::vector` to store temporary `Move` objects. Additionally, a `MoveConstructor.exe.recipe` file was introduced to define metadata for the project, such as the output path for the compiled executable.

--- Project_examples/MoveConstructor/Move.cpp
+++ Project_examples/MoveConstructor/Move.cpp
@@ -5,7 +5,7 @@ int Move::getDataValue() const { return *data; }
 int* Move::getDataAddress() const { return data; }
 Move::Move(int d) { data = new int; *data = d; std::cout << "\nConstructor for: " << d << std::endl; }
 Move::Move(const Move& source) : Move(*source.data) { std::cout << std::endl << "Copy constructor - deep copy for: " << *data << std::endl; }
-//Move::Move(Move&& source) : data(source.data) { source.data = nullptr; std::cout << "\nMove constructor - moving resource: " << *data << std::endl; }
+Move::Move(Move&& source) : data(source.data) { source.data = nullptr; std::cout << "\nMove constructor - moving resource: " << *data << std::endl; }
 Move::~Move()
 { 
 	if (data != nullptr)
---
+++ Project_examples/MoveConstructor/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include "Move.h"
+
+int main()
+{
+	std::vector<Move> vec;
+
+	vec.push_back(Move{ 10 });
+	vec.push_back(Move{ 20 });
+	vec.push_back(Move{ 30 });
+	vec.push_back(Move{ 40 });
+	vec.push_back(Move{ 50 });
+	vec.push_back(Move{ 60 });
+	vec.push_back(Move{ 70 });
+	vec.push_back(Move{ 80 });
+
+	std::cout << "\n\t--- End Program ---\n";
+	std::cin.get();
+	return 0;
+}
