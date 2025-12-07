83dc741b4cc2e498ca246372ba91f9d59c18a3b2
Belmy Maripane <bsmaripane@gmail.com>
2025-12-07 21:54:59 +0200

    Definition of the static method

--- Project_examples/CopyConstructor/Player.cpp
+++ Project_examples/CopyConstructor/Player.cpp
@@ -1,6 +1,6 @@
 #include "Player.h"
 
-Player::Player(std::string pName, int pHealth, int pXp) : name(pName), health(pHealth), xp(pXp) {}
+Player::Player(std::string pName, int pHealth, int pXp) : name(pName), health(pHealth), xp(pXp) { ++numPlayers; }
 
 void Player::setName(std::string pName) { name = pName; }
 void Player::setHealth(int pHealth) { health = pHealth; }
@@ -9,7 +9,7 @@ void Player::setXp(int pXp) { xp = pXp; }
 std::string Player::getName() const { return name; }
 int Player::getHealth() const { return health; }
 int Player::getXp() const { return xp; }
-
-Player::Player(const Player& source) : name(source.name), health(source.health), xp(source.xp) { std::cout << "Copy constructor - made of: " << source.name << std::endl; }
+int Player::getNumPlayers() { return numPlayers; }
+Player::Player(const Player& source) : name(source.name), health(source.health), xp(source.xp) { ++numPlayers; std::cout << "Copy constructor - made of: " << source.name << std::endl; }
 
 Player::~Player() { std::cout << "\nDeallocating the memory!\n"; }
