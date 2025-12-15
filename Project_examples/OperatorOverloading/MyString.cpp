239cfc77748da0d2652eda9c73f8ac5d48ff528d
Belmy Maripane <bsmaripane@gmail.com>
2025-12-15 12:02:23 +0200

    Implement the constructors

--- Project_examples/OperatorOverloading/MyString.cpp
+++ Project_examples/OperatorOverloading/MyString.cpp
@@ -1,15 +1,29 @@
 #include "MyString.h"
 
-MyString::MyString()
+MyString::MyString() : str(nullptr)
 {
+	str = new char[1];
+	*str = '\0';
 }
 
-MyString::MyString(const char* s)
+MyString::MyString(const char* s) : str(nullptr)
 {
+	if (s == nullptr)
+	{
+		str = new char[1];
+		*str = '\0';
+	}
+	else
+	{
+		str = new char[std::strlen(s) + 1];
+		std::strcpy(str, s);
+	}
 }
 
-MyString::MyString(const MyString& source)
+MyString::MyString(const MyString& source) : str(nullptr)
 {
+	str = new char[std::strlen(source.str) + 1];
+	std::strcpy(str, source.str);
 }
 
 MyString::~MyString()
