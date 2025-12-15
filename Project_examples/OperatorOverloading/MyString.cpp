e0710707a9857c07df9ee9babdb52f97da8f5462
Belmy Maripane <bsmaripane@gmail.com>
2025-12-15 11:41:15 +0200

    Add declaration and definition of the MyString class

---
+++ Project_examples/OperatorOverloading/MyString.cpp
@@ -0,0 +1,31 @@
+#include "MyString.h"
+
+MyString::MyString()
+{
+}
+
+MyString::MyString(const char* s)
+{
+}
+
+MyString::MyString(const MyString& source)
+{
+}
+
+MyString::~MyString()
+{
+}
+
+void MyString::display() const
+{
+}
+
+int MyString::getLength() const
+{
+	return 0;
+}
+
+const char* MyString::getStr() const
+{
+	return nullptr;
+}
---
+++ Project_examples/OperatorOverloading/MyString.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef MYSTRING_H
+#define MYSTRING_H
+#include <iostream>
+
+class MyString
+{
+private:
+	char* str;		// C-style string
+
+public:
+	MyString();
+	MyString(const char* s);
+	MyString(const MyString& source);
+	~MyString();
+	void display() const;
+	int getLength() const;
+	const char* getStr() const;
+};
+#endif
