c31a58da268b3005cc95220284286aa2d5c1a130
Belmy Maripane <bsmaripane@gmail.com>
2025-12-22 20:41:15 +0200

    Add percentage sign for console

--- Project_examples/Inheritance_DerivingClass/Savings_Account.cpp
+++ Project_examples/Inheritance_DerivingClass/Savings_Account.cpp
@@ -10,6 +10,6 @@ bool Savings_Account::deposit(double amount)
 
 std::ostream& operator<<(std::ostream& os, const Savings_Account& account)
 {
-	os << "[ Saving account balance: " << std::fixed << std::setprecision(2) << account.balance << " interest rate: " << std::fixed << std::setprecision(3) << account.int_rate  << " ]" << std::endl;
+	os << "[ Saving account balance: " << std::fixed << std::setprecision(2) << account.balance << " interest rate: " << std::fixed << std::setprecision(3) << account.int_rate  << "% ]" << std::endl;
 	return os;
 }
