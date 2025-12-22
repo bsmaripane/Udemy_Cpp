0df66aa92ebb1ec5a1422332ce6a173b46e3b230
Belmy Maripane <bsmaripane@gmail.com>
2025-12-22 12:57:08 +0200

    Simple project using Constructors and Destructors

---
+++ Project_examples/Inheritance_ConstructorsAndDestructors/main.cpp
@@ -0,0 +1,33 @@
+// Section 15 Constructors and Destructors
+
+#include <iostream>
+
+class Base
+{
+public:
+	Base() : value{ 0 } { std::cout << "Base no-args constructor" << std::endl; }
+	Base(int x) : value{ x } { std::cout << "Base (int) overloaded constructor" << std::endl; }
+	~Base() { std::cout << "Base destructor" << std::endl; }
+
+private:
+	int value;
+};
+
+class Derived : public Base
+{
+private:
+	int doubled_value;
+public:
+	Derived() : doubled_value{ 0 } { std::cout << "Derived no-args constructor" << std::endl; }
+	Derived(int x) : doubled_value{ x*2 } { std::cout << "Derived (int) overloaded constructor" << std::endl; }
+	~Derived() { std::cout << "Derived destructor" << std::endl; }
+};
+
+int main() 
+{
+	Base base;
+	Base b{ 200 };
+
+	std::cin.get();
+	return 0;
+}
