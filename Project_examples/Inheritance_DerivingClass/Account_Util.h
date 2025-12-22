b915e5f79ba44faa8319f92aad62b64951a9fefc
Belmy Maripane <bsmaripane@gmail.com>
2025-12-22 18:26:41 +0200

    Delete Account_Util.cpp and move the definition into the .h and use template

--- Project_examples/Inheritance_DerivingClass/Account_Util.h
+++ Project_examples/Inheritance_DerivingClass/Account_Util.h
@@ -5,14 +5,33 @@
 #include "Account.h"
 #include "Savings_Account.h"
 
-// Utility helper functions for Account class
-void display(const std::vector<Account>& accounts);
-void deposit(std::vector<Account>& accounts, double amount);
-void withdraw(std::vector<Account>& accounts, double amount);
+// Utility helper functions
+template<class Type>
+void display(const std::vector<Type>& accounts)
+{
+	std::cout << "\n================ Account Details ================================================\n";
+	for (const auto& acc : accounts)
+		std::cout << acc << std::endl;
+}
 
-// Utility helper functions for Savings Account class
-void display(const std::vector<Savings_Account>& accounts);
-void deposit(std::vector<Savings_Account>& accounts, double amount);
-void withdraw(std::vector<Savings_Account>& accounts, double amount);
+template<class Type>
+void deposit(std::vector<Type>& accounts, double amount)
+{
+	std::cout << "\n================ Depositing to ==================================================\n";
+	for (auto& acc : accounts)
+		if (acc.deposit(amount))
+			std::cout << "Deposited R" << std::fixed << std::setprecision(2) << amount << " to " << acc << std::endl;
+		else
+			std::cout << "Failed Deposit of R" << std::fixed << std::setprecision(2) << amount << " to " << acc << std::endl;
+}
 
+template<class Type>
+void withdraw(std::vector<Type>& accounts, double amount) {
+	std::cout << "\n================ Withdrawing from ===============================================\n";
+	for (auto& acc : accounts)
+		if (acc.withdraw(amount))
+			std::cout << "Withdraw R" << std::fixed << std::setprecision(2) << amount << " from " << acc << std::endl;
+		else
+			std::cout << "Failed Withdrawal of R" << std::fixed << std::setprecision(2) << amount << " from " << acc << std::endl;
+}
 #endif // ACCOUNT_UTIL_H
