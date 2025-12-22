e46693e39330c1e2d407f1bc1ef4dec3ea9ad40d
Belmy Maripane <bsmaripane@gmail.com>
2025-12-22 16:20:17 +0200

    Update the saving account derived and rename the class

---
+++ Project_examples/Inheritance_DerivingClass/Savings_Account.cpp
@@ -0,0 +1,15 @@
+#include "Savings_Account.h"
+
+Savings_Account::Savings_Account(std::string nmae, double balance, double int_rate) : Account{name, balance}, int_rate(int_rate) {}
+
+bool Savings_Account::deposit(double amount)
+{
+	amount += (amount * int_rate / 100);
+	return Account::deposit(amount);
+}
+
+std::ostream& operator<<(std::ostream& os, const Savings_Account& account)
+{
+	os << "[ Saving account balance: " << std::fixed << std::setprecision(2) << account.balance << " interest rate: " << std::fixed << std::setprecision(3) << account.int_rate  << " ]" << std::endl;
+	return os;
+}
---
+++ Project_examples/Inheritance_DerivingClass/Savings_Account.h
@@ -0,0 +1,21 @@
+#ifndef SAVINGS_ACCOUNT_H
+#define SAVINGS_ACCOUNT_H
+
+#include "Account.h"
+
+// Saving Account is a type of Account adds and interest rate.
+// Deposit: Amount supplied to deposit will be incremented by (amount *int_rate/100) and then the updated amount will be deposited
+class Savings_Account : public Account
+{
+	friend std::ostream& operator<<(std::ostream& os, const Savings_Account& account);
+private:
+	static constexpr const char* def_name{ "Default Saving Account" };
+	static constexpr double def_balance{ 0.0 };
+	static constexpr double def_int_rate{ 0.0 };
+protected:
+	double int_rate;
+public:
+	Savings_Account(std::string nmae = def_name, double balance = def_balance, double int_rate = def_int_rate);
+	bool deposit(double amount);
+};
+#endif // SAVINGS_ACCOUNT_H
