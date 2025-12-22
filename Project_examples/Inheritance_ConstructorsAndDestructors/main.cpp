fc65c9180e5da650675e1cdfc724c48d04d51a17
Belmy Maripane <bsmaripane@gmail.com>
2025-12-22 13:04:54 +0200

    to use Base(int x) constructor

--- Project_examples/Inheritance_ConstructorsAndDestructors/main.cpp
+++ Project_examples/Inheritance_ConstructorsAndDestructors/main.cpp
@@ -15,11 +15,12 @@ private:
 
 class Derived : public Base
 {
+	using Base::Base;	// to use Base(int x) constructor
 private:
 	int doubled_value;
 public:
 	Derived() : doubled_value{ 0 } { std::cout << "Derived no-args constructor" << std::endl; }
-	Derived(int x) : doubled_value{ x*2 } { std::cout << "Derived (int) overloaded constructor" << std::endl; }
+	//Derived(int x) : doubled_value{ x*2 } { std::cout << "Derived (int) overloaded constructor" << std::endl; }
 	~Derived() { std::cout << "Derived destructor" << std::endl; }
 };
 
