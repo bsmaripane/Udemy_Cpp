ac0ddd6284445308f36ac4608d7917f528568fbc
Belmy Maripane <bsmaripane@gmail.com>
2025-12-22 10:24:02 +0200

    Add string to print when calling the class methods

--- Project_examples/Inheritance_DerivingClass/Account.cpp
+++ Project_examples/Inheritance_DerivingClass/Account.cpp
@@ -2,16 +2,14 @@
 
 void Account::deposit(double amount)
 {
+	std::cout << "Account deposit called with " << amount << std::endl;
 }
 
 void Account::withdraw(double amount)
 {
+	std::cout << "Account withdraw called with " << amount << std::endl;
 }
 
-Account::Account()
-{
-}
+Account::Account() {}
 
-Account::~Account()
-{
-}
+Account::~Account() {}
