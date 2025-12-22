991bd730df5fa84f28f54b496a8676901b1c2fd5
Belmy Maripane <bsmaripane@gmail.com>
2025-12-22 13:43:18 +0200

    Test the derived of the base copy constructor and overloaded operator

--- Project_examples/Inheritance_ConstructorsAndDestructors/main.cpp
+++ Project_examples/Inheritance_ConstructorsAndDestructors/main.cpp
@@ -24,12 +24,21 @@ private:
 
 class Derived : public Base
 {
-	using Base::Base;	// to use Base(int x) constructor
 private:
 	int doubled_value;
 public:
 	Derived() : Base{}, doubled_value { 0 } { std::cout << "Derived no-args constructor" << std::endl; }
 	Derived(int x) : Base{ x }, doubled_value{ x * 2 } { std::cout << "Derived (int) overloaded constructor" << std::endl; }
+	Derived(const Derived& other) : Base(other), doubled_value{ other.doubled_value } { std::cout << "Derived copy constructor" << std::endl; }
+	Derived& operator=(const Derived& rhs)
+	{
+		std::cout << "Derived operator=" << std::endl;
+		if (this == &rhs)
+			return *this;
+		Base::operator=(rhs);
+		doubled_value = rhs.doubled_value;
+		return *this;
+	}
 	~Derived() { std::cout << "Derived destructor" << std::endl; }
 };
 
@@ -39,8 +48,11 @@ int main()
 	Base b{ 100 };
 	Base b1{ b };
 	b = b1;
-	//Derived derived;
-	//Derived d{ 1000 };
+	std::cout << std::endl;
+	Derived derived;
+	Derived d{ 1000 };
+	Derived d2{ d };
+	d = d2;
 
 	std::cin.get();
 	return 0;
