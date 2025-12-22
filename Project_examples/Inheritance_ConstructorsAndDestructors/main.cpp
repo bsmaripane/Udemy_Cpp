238532e8d33e6838df1eb7db605dd798f56ec29f
Belmy Maripane <bsmaripane@gmail.com>
2025-12-22 13:05:20 +0200

    use both

--- Project_examples/Inheritance_ConstructorsAndDestructors/main.cpp
+++ Project_examples/Inheritance_ConstructorsAndDestructors/main.cpp
@@ -20,7 +20,7 @@ private:
 	int doubled_value;
 public:
 	Derived() : doubled_value{ 0 } { std::cout << "Derived no-args constructor" << std::endl; }
-	//Derived(int x) : doubled_value{ x*2 } { std::cout << "Derived (int) overloaded constructor" << std::endl; }
+	Derived(int x) : doubled_value{ x*2 } { std::cout << "Derived (int) overloaded constructor" << std::endl; }
 	~Derived() { std::cout << "Derived destructor" << std::endl; }
 };
 
