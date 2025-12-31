841325e73b8c6d765d0f8f523c40ac345d4a30cd
Belmy Maripane <bsmaripane@gmail.com>
2025-12-31 16:54:38 +0200

    Static binding

---
+++ Project_examples/Section_16_The_Problem/main.cpp
@@ -0,0 +1,40 @@
+// Section 16 The Problem
+
+#include <iostream>
+
+class Base
+{
+public:
+	void say_hello() const
+	{
+		std::cout << "Hello - I'm a Base class object" << std::endl;
+	}
+};
+
+class Derived : public Base
+{
+public:
+	void say_hello() const
+	{
+		std::cout << "Hello - I'm a Derived class object" << std::endl;
+	}
+};
+
+void greetings(const Base& obj)
+{
+	std::cout << "Greetings: ";
+	obj.say_hello();
+}
+
+int main()
+{
+	// Static binding
+	Base b;
+	b.say_hello();
+
+	Derived d;
+	d.say_hello();
+
+	std::cin.get();
+	return 0;
+}
