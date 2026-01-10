3bc76743e0ebec6305dc88ecde625658070210c0
Belmy Maripane <bsmaripane@gmail.com>
2026-01-10 15:50:19 +0200

    To use dynamic polymorphism to match the appropriate one

--- Project_examples/Sec_16_AbstractClassesAsInterfaces/main.cpp
+++ Project_examples/Sec_16_AbstractClassesAsInterfaces/main.cpp
@@ -2,62 +2,52 @@
 // Interface - start
 
 #include <iostream>
-
-class Account
+class I_Printable
 {
-	friend std::ostream& operator<<(std::ostream& os, const Account& acc);
+	friend std::ostream& operator<<(std::ostream& os, const I_Printable& obj);
+public:
+	virtual void print(std::ostream& os) const = 0;
+};
+
+std::ostream& operator<<(std::ostream& os, const I_Printable& obj)
+{
+	obj.print(os);
+	return os;
+}
+
+class Account : public I_Printable
+{
 public:
 	virtual void withdraw(double amount) { std::cout << "In Account::withdraw" << std::endl; }
+	virtual void print(std::ostream& os) const override { os << "Account display"; }
 	virtual ~Account() {}
 };
 
-std::ostream& operator<<(std::ostream& os, const Account& acc)
-{
-	os << "Account display";
-	return os;
-}
-
 class Checking : public Account
 {
-	friend std::ostream& operator<<(std::ostream& os, const Checking& acc);
 public:
 	virtual void withdraw(double amount) { std::cout << "In Checking::withdraw" << std::endl; }
+	virtual void print(std::ostream& os) const override { os << "Checking display"; }
 	virtual ~Checking() {}
 };
 
-std::ostream& operator<<(std::ostream& os, const Checking& acc)
-{
-	os << "Checking display";
-	return os;
-}
 
 class Savings : public Account
 {
-	friend std::ostream& operator<<(std::ostream& os, const Savings& acc);
 public:
 	virtual void withdraw(double amount) { std::cout << "In Savings::withdraw" << std::endl; }
+	virtual void print(std::ostream& os) const override { os << "Savings display"; }
 	virtual ~Savings() {}
 };
 
-std::ostream& operator<<(std::ostream& os, const Savings& acc)
-{
-	os << "Savings display";
-	return os;
-}
-
 class Trust : public Account
 {
-	friend std::ostream& operator<<(std::ostream& os, const Trust& acc);
 public:
 	virtual void withdraw(double amount) { std::cout << "In Trust::withdraw" << std::endl; }
+	virtual void print(std::ostream& os) const override { os << "Trust display"; }
 	virtual ~Trust() {}
 };
 
-std::ostream& operator<<(std::ostream& os, const Trust& acc)
-{
-	os << "Trust display";
-	return os;
-}
 
 int main()
 {
