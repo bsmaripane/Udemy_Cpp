1edeb8fe1a63d9e296a522fce2396f5ad26b35a2
Belmy Maripane <bsmaripane@gmail.com>
2026-01-10 16:36:23 +0200

    Add the defaual for destructor

--- Project_examples/Section_16_Polymorphism_Section_Challaenge/Account.h
+++ Project_examples/Section_16_Polymorphism_Section_Challaenge/Account.h
@@ -17,5 +17,6 @@ public:
 	Account(std::string name = def_name, double balance = def_balance);
 	virtual bool deposit(double amount) = 0;
 	virtual bool withdraw(double amount) = 0;
+	virtual ~Account() = default;
 };
 #endif //ACCOUNT_H
