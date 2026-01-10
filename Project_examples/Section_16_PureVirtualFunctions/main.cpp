4a66ffd56cf7ced21ab07e1d70851fbd58ab34b4
Belmy Maripane <bsmaripane@gmail.com>
2026-01-10 14:25:46 +0200

    It draw a circle using the polymorphism

---
+++ Project_examples/Section_16_PureVirtualFunctions/main.cpp
@@ -0,0 +1,64 @@
+// Section 16 Pure virtual functions and abstract base classes
+
+#include <iostream>
+#include <vector>
+
+class Shape		// Abstract Base Class
+{
+private:
+	// attributes common to all shapes
+
+public:
+	virtual void draw() = 0;	// pure virtual function
+	virtual void rotate() = 0;	// pure virtual function
+	virtual ~Shape() {}
+};
+
+class Open_Shape : public Shape		// Abstract class
+{
+public:
+	virtual ~Open_Shape() { }
+};
+
+class Closed_Shape : public Shape	// Abstract class
+{
+public:
+	virtual ~Closed_Shape() { }
+};
+
+class Line : public Open_Shape	// Concrete class
+{
+public:
+	virtual void draw() override { std::cout << "Drawing a line" << std::endl; }
+	virtual void rotate() override { std::cout << "Rotation a line" << std::endl; }
+	virtual ~Line() {}
+};
+
+class Circle : public Closed_Shape	// Concrete class
+{
+public:
+	virtual void draw() override { std::cout << "Drawing a circle" << std::endl; }
+	virtual void rotate() override { std::cout << "Rotating a circle" << std::endl; }
+	virtual ~Circle() {}
+};
+
+class Square : public Closed_Shape	// Concrete class
+{
+public:
+	virtual void draw() override { std::cout << "Drawing a square" << std::endl; }
+	virtual void rotate() override { std::cout << "Rotating a square" << std::endl; }
+	virtual ~Square() {}
+};
+
+int main()
+{
+	//Shape s;
+	//Shape* p = new Shape();
+	//s.draw();
+
+	Circle c;
+	c.draw();
+
+	std::cin.get();
+	return 0;
+}
