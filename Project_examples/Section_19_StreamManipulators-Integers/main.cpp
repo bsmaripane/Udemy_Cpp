85cc09b1e23c6e7edda08812da88665d2ab6e1cc
Belmy Maripane <bsmaripane@gmail.com>
2026-01-11 19:29:41 +0200

    To show the positive sign

--- Project_examples/Section_19_StreamManipulators-Integers/main.cpp
+++ Project_examples/Section_19_StreamManipulators-Integers/main.cpp
@@ -7,11 +7,15 @@ int main()
 {
 	// Formatting integer types - setting base
 	int num{ 255 };
+	int num1{ -255 };
 
-	std::cout << "std::showbase and std::uppercase" << std::showbase << std::uppercase;
+	std::cout << "std::showbase, std::showpos and std::uppercase" << std::showbase << std::showpos << std::uppercase;
 	std::cout << "std::dec : 255 is " << std::dec << num << std::endl;
+	std::cout << "std::dec : 255 is " << std::dec << num1 << std::endl;
 	std::cout << "std::hex : 255 is " << std::hex << num << std::endl;
+	std::cout << "std::hex : 255 is " << std::hex << num1 << std::endl;
 	std::cout << "std::oct : 255 is " << std::oct << num << std::endl;
+	std::cout << "std::oct : 255 is " << std::oct << num1 << std::endl;
 
 	system("pause");
 	return 0;
