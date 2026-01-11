aa2545da45ac3dec1030fac977b5858d8d186268
Belmy Maripane <bsmaripane@gmail.com>
2026-01-11 15:46:25 +0200

    Add the multple exception handing and catching any exception clause

--- Project_examples/Section_18_ExceptionHandling/main.cpp
+++ Project_examples/Section_18_ExceptionHandling/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-
+#include <string>
 
 double calculate_avg(int sum, int total)
 {
 	if (total == 0)
 		throw 0;
+	if (sum < 0 || total < 0)
+		throw std::string{ "Negative value error" };
 	return static_cast<double>(sum) / total;
 }
 
@@ -12,7 +14,9 @@ int main()
 {
 	int numerator{}, denominator{};
 	double result{};
+	double results{};
 	double average{};
+
 
 	std::cout << "Enter a numerator: ";
 	std::cin >> numerator;
@@ -30,6 +34,8 @@ int main()
 			result = static_cast<double>(numerator) / denominator;
 			std::cout << "Result: " << result << std::endl;
 		}
+		results = calculate_avg(numerator, denominator);
+		std::cout << "Results: " << results << std::endl;
 		// Catching an exception thrown from a function
 		average = calculate_avg((67 + 79 + 91 + 59 + 54), 500);
 		std::cout << "Average: " << average << std::endl;
@@ -37,6 +43,14 @@ int main()
 	catch (int& ex)
 	{
 		std::cerr << "Error - you can't divide by zero" << std::endl;
+	}
+	catch (std::string& ex)
+	{
+		std::cerr << ex << std::endl;
+	}
+	catch (...)
+	{
+		std::cerr << "Unknown exception occurred!" << std::endl;
 	}
 
 	
