797679b91e1991761c91018624dee0e94aeaf128
Belmy Maripane <bsmaripane@gmail.com>
2026-01-11 19:38:07 +0200

    Add the setting and resetting integer types

--- Project_examples/Section_19_StreamManipulators-Integers/main.cpp
+++ Project_examples/Section_19_StreamManipulators-Integers/main.cpp
@@ -1,7 +1,6 @@
 // Section 19: Stream Manipulators - Integers
 
 #include <iostream>
-#include <format>
 
 int main()
 {
@@ -9,13 +8,24 @@ int main()
 	int num{ 255 };
 	int num1{ -255 };
 
-	std::cout << "std::showbase, std::showpos and std::uppercase" << std::showbase << std::showpos << std::uppercase;
+	std::cout << "std::showbase, std::showpos and std::uppercase" << std::showbase << std::showpos << std::uppercase << std::endl;
 	std::cout << "std::dec : 255 is " << std::dec << num << std::endl;
 	std::cout << "std::dec : 255 is " << std::dec << num1 << std::endl;
 	std::cout << "std::hex : 255 is " << std::hex << num << std::endl;
 	std::cout << "std::hex : 255 is " << std::hex << num1 << std::endl;
 	std::cout << "std::oct : 255 is " << std::oct << num << std::endl;
 	std::cout << "std::oct : 255 is " << std::oct << num1 << std::endl;
+
+	// Set using setf
+	std::cout.setf(std::ios::showbase);
+	std::cout.setf(std::ios::uppercase);
+	std::cout.setf(std::ios::showpos);
+
+	// Reset to defaults
+	std::cout << std::resetiosflags(std::ios::basefield);
+	std::cout << std::resetiosflags(std::ios::showbase);
+	std::cout << std::resetiosflags(std::ios::showpos);
+	std::cout << std::resetiosflags(std::ios::uppercase);
 
 	system("pause");
 	return 0;
