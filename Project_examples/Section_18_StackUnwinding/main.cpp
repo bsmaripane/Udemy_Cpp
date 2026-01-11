d3b46d921a9dd94fcefb2eaef32ba173287c65fa
Belmy Maripane <bsmaripane@gmail.com>
2026-01-11 16:09:39 +0200

    Add a simple stack unwinding without exception handling

---
+++ Project_examples/Section_18_StackUnwinding/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void func_b();
+void func_c();
+
+int main()
+{
+	std::cout << "Starting main" << std::endl;
+	func_a();
+
+	std::cout << "Finishing main" << std::endl;
+	std::cout << "\n\t= = = = = = End of Program = = = = = =";
+	system("pause");
+	return 0;
+}
+
+
+void func_a()
+{
+	std::cout << "Starting func_a" << std::endl;
+	func_b();
+	std::cout << "Ending func_a" << std::endl;
+}
+
+void func_b()
+{
+	std::cout << "Starting func_b" << std::endl;
+	func_c();
+	std::cout << "Ending func_b" << std::endl;
+}
+
+void func_c()
+{
+	std::cout << "Starting func_c" << std::endl;
+	std::cout << "Ending func_c" << std::endl;
+}
