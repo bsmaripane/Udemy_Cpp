25db707960abf4ae597ab210c0183001c1327252
Belmy Maripane <bsmaripane@gmail.com>
2026-01-17 16:19:35 +0200

    Display the scientific uppercase

--- Project_examples/StreamManipulators_FloatingPoint/main.cpp
+++ Project_examples/StreamManipulators_FloatingPoint/main.cpp
@@ -5,11 +5,11 @@
 
 int main()
 {
-	double num{ 1234.5678 };
+	double num{ 123456789.987654321 };
 
 	std::cout << "num: " << num << std::endl;
 
-	std::cout << "num: " << std::fixed << std::setprecision(4) << num << std::endl;
+	std::cout << "num: " << std::fixed << std::setprecision(4) << std::scientific << std::uppercase << num << std::endl;
 
 	system("pause>0");
 	return 0;
