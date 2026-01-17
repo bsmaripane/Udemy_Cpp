debfb7c0cb8f899a548f6f4d6e3839f287e0dbd0
Belmy Maripane <bsmaripane@gmail.com>
2026-01-17 18:15:43 +0200

    Filling fixed width - setfill

--- Project_examples/StreamManipulators_AlignAndFill/main.cpp
+++ Project_examples/StreamManipulators_AlignAndFill/main.cpp
@@ -14,8 +14,10 @@ int main()
 
 	// Filling Fixed width - setw(10) and right 
 	std::cout << "-- Filling fixed width - setfill -------------" << std::endl;
-	std::cout << std::setfill('-');
-	std::cout << "Setw(10): " << std::setw(10) << num1 << name << std::endl;
+	std::cout << std::setfill('*');
+	std::cout << "Setw(10): " << std::setw(10) << num1 
+							  << std::setfill('-' ) << std::setw(10) << std::right << name
+							  << std::setw(15) << std::right << name << std::endl;
 
 	system("pause>0");
 	return 0;
