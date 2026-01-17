022b04e21607ac16950b8af8b79e1d8c7b0372bc
Belmy Maripane <bsmaripane@gmail.com>
2026-01-17 16:26:36 +0200

    Ddisplaying the trailing zeros

--- Project_examples/StreamManipulators_FloatingPoint/main.cpp
+++ Project_examples/StreamManipulators_FloatingPoint/main.cpp
@@ -5,11 +5,11 @@
 
 int main()
 {
-	double num{ 123456789.987654321 };
+	double num{ 12.34 };
 
 	std::cout << "num: " << num << std::endl;
 
-	std::cout << std::setprecision(4) << std::fixed << std::showpos;
+	std::cout << std::showpoint;
 	std::cout << "num: " << num << std::endl;
 
 	system("pause>0");
