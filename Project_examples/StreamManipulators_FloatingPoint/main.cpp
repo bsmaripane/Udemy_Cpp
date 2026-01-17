5314841b03f14c26d1bd5c28637631611e157cc6
Belmy Maripane <bsmaripane@gmail.com>
2026-01-17 16:24:35 +0200

    Displaying the positive sign

--- Project_examples/StreamManipulators_FloatingPoint/main.cpp
+++ Project_examples/StreamManipulators_FloatingPoint/main.cpp
@@ -9,7 +9,8 @@ int main()
 
 	std::cout << "num: " << num << std::endl;
 
-	std::cout << "num: " << std::fixed << std::setprecision(4) << std::scientific << std::uppercase << num << std::endl;
+	std::cout << std::setprecision(4) << std::fixed << std::showpos;
+	std::cout << "num: " << num << std::endl;
 
 	system("pause>0");
 	return 0;
