e39d9d8657e7f0a1eb7dedfcc7d70bab828ddd35
Belmy Maripane <bsmaripane@gmail.com>
2026-01-17 17:40:23 +0200

    Show trailing zeros up to precision 10

--- Project_examples/StreamManipulators_FloatingPoint/main.cpp
+++ Project_examples/StreamManipulators_FloatingPoint/main.cpp
@@ -23,6 +23,17 @@ int main()
 	std::cout << "num2: " << num2 << std::endl;
 	std::cout << "num3: " << num3 << std::endl;
 
+	// Back to defaults
+	std::cout.unsetf(std::ios::scientific | std::ios::fixed);
+	std::cout << std::resetiosflags(std::ios::showpos);
+
+	// Show trailing zeros up to precision 10
+	std::cout << std::setprecision(3) << std::fixed << std::showpoint;
+	std::cout << "--- Precision 10 - showpoint ---------------" << std::endl;
+	std::cout << "num1: " << num1 << std::endl;
+	std::cout << "num2: " << num2 << std::endl;
+	std::cout << "num3: " << num3 << std::endl;
+
 	system("pause>0");
 	return 0;
 }
