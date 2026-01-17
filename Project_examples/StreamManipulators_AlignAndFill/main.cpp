bad42512aaebc504b1ea9d9bba2f3c578090bd15
Belmy Maripane <bsmaripane@gmail.com>
2026-01-17 18:13:08 +0200

    Filling Fixed width - setw(10) and right

--- Project_examples/StreamManipulators_AlignAndFill/main.cpp
+++ Project_examples/StreamManipulators_AlignAndFill/main.cpp
@@ -12,11 +12,10 @@ int main()
 	std::cout << "-- Defaults ----------------------------------" << std::endl;
 	std::cout << "Default: " << num1 << name << std::endl;
 
-	// Using Field width - justification - setw(10) and right 
-	std::cout << "-- Field width - Setw(10) and right -----------" << std::endl;
-	std::cout << "Setw(10): " << std::setw(10) << num1 
-							  << std::setw(10) << std::right << name 
-							  << std::setw(15) << std::right << name << std::endl;
+	// Filling Fixed width - setw(10) and right 
+	std::cout << "-- Filling fixed width - setfill -------------" << std::endl;
+	std::cout << std::setfill('-');
+	std::cout << "Setw(10): " << std::setw(10) << num1 << name << std::endl;
 
 	system("pause>0");
 	return 0;
