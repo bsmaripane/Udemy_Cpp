4369e9f121b7352ef879072cd5dd1c0f3cede8ee
Belmy Maripane <bsmaripane@gmail.com>
2026-01-17 17:53:59 +0200

    Using the defaults

---
+++ Project_examples/StreamManipulators_AlignAndFill/main.cpp
@@ -0,0 +1,16 @@
+// Stream Manipulator
+
+#include <iostream>
+
+int main()
+{
+	double num1{ 1234.5678 };
+	std::string name{ "Belicia" };
+
+	// Default
+	std::cout << "-- Defaults ----------------------------------" << std::endl;
+	std::cout << "Default: " << num1 << name << std::endl;
+
+	system("pause>0");
+	return 0;
+}
