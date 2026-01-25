1e1442271bdf3b5233ca33e6abb241e365891ef7
Belmy Maripane <bsmaripane@gmail.com>
2026-01-25 18:37:32 +0200

    Refactor file open logic to improve exception handling
    Moved file open failure check and error handling inside the try block, ensuring exceptions are properly caught and reported. This enhances the structure and exception safety of the file opening process.

--- Project_examples/Section_19_ReadAndDisplayPoem/Section_19_ReadAndDisplayPoem/main.cpp
+++ Project_examples/Section_19_ReadAndDisplayPoem/Section_19_ReadAndDisplayPoem/main.cpp
@@ -10,16 +10,18 @@ int main()
 	try
 	{
 		in_file.open("../poem.txt");
-	}
-	catch (const std::exception& ex)
-	{
-		std::cerr << "Error: " << ex.what() << std::endl;
 		if (!in_file.is_open())
 		{
 			throw std::ios_base::failure("Failed to open the file.");
 			system("pause>0");
 			return 1;
 		}
+
+
+	}
+	catch (const std::exception& ex)
+	{
+		std::cerr << "Error: " << ex.what() << std::endl;
 	}
 
 	if (in_file.is_open()) in_file.close();
