4f3e81faf58bf1afaa97468a88810e51b625c241
Belmy Maripane <bsmaripane@gmail.com>
2026-01-25 18:21:01 +0200

    The improvements include better error handling, ensuring the file is properly closed even in case of an exception, and adding functionality to read and display the poem using std::getline.

---
+++ Project_examples/Section_19_ReadAndDisplayPoem/Section_19_ReadAndDisplayPoem/main.cpp
@@ -0,0 +1,26 @@
+// Section 19 Read and Display Shakespeare Sonnet 19 poem using getline
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+	std::ifstream in_file;
+
+	try
+	{
+		in_file.open("../poem.txt");
+	}
+	catch (const std::exception& ex)
+	{
+		std::cerr << "Error: " << ex.what() << std::endl;
+		if (!in_file.is_open())
+		{
+			throw std::ios_base::failure("Failed to open the file.");
+		}
+	}
+
+	if (in_file.is_open()) in_file.close();
+	system("pause>0");
+	return 0;
+}
