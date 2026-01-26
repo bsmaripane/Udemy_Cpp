b847428b0a35510dcb2138cab0fc87a15cb40fe8
Belmy Maripane <bsmaripane@gmail.com>
2026-01-26 13:58:29 +0200

    Add a validing input with stingstream

--- Project_examples/Section_19_UsingStringStream/Section_19_UsingStringStream/main.cpp
+++ Project_examples/Section_19_UsingStringStream/Section_19_UsingStringStream/main.cpp
@@ -16,6 +16,19 @@ int main()
 
 	std::cout << oss.str() << std::endl;
 
+	// Validing input with stingstream
+	int value{};
+	std::string input{};
+
+	std::cout << "Enter an integer: ";
+	std::cin >> input;
+
+	std::stringstream ss{ input };
+	if (ss >> value)
+		std::cout << "An integer was entered";
+	else
+		std::cout << "An integer was NOT entered";
+
 	system("pause>0");
 	return 0;
 }
