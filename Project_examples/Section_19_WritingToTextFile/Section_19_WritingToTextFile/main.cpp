e0308f850d1b80ffeaa41279f08d373655839fca
Belmy Maripane <bsmaripane@gmail.com>
2026-01-26 11:42:07 +0200

    Add exception handling for writting to a file

---
+++ Project_examples/Section_19_WritingToTextFile/Section_19_WritingToTextFile/main.cpp
@@ -0,0 +1,28 @@
+// Section 19 Writing to a Text File
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+	std::string fileName{}; 
+	std::ofstream outFile{};
+
+	try
+	{
+		if (!outFile)
+		{
+			std::cerr << "Failed to open: " << fileName << std::endl;
+			system("pause>0");
+			return 1;
+		}
+	}
+	catch (std::exception& ex)
+	{
+		std::cerr << "Exception error: " << ex.what() << std::endl;
+	}
+
+	system("pause>0");
+	return 0;
+}
