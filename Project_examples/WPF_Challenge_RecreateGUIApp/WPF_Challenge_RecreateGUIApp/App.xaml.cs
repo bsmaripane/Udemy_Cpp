532eaf5d3159275257aa495ee438328eaa62cb9a
Belmy Maripane <bsmaripane@gmail.com>
2026-02-17 20:00:35 +0200

    First GUI challenge

---
+++ Project_examples/WPF_Challenge_RecreateGUIApp/WPF_Challenge_RecreateGUIApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_Challenge_RecreateGUIApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
