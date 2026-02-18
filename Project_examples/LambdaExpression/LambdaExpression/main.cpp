34bbba0353b0c374a057fb252121cf0bec1194e0
Belmy Maripane <bsmaripane@gmail.com>
2026-02-18 07:09:25 +0200

    Returning a value from a lambda expression

--- Project_examples/LambdaExpression/LambdaExpression/main.cpp
+++ Project_examples/LambdaExpression/LambdaExpression/main.cpp
@@ -8,6 +8,15 @@ int main()
 
 	word();
 
+	// Returning a value from a lambda expression
+	auto product = [](int x, int y) -> int { return x * y; };
+
+	std::cout << product(4, 5) << std::endl;
+
+	auto module = [](int a, int b) { return a % b; };
+
+	std::cout << module(9, 5) << std::endl;
+
 	system("pause");
 	return 0;
 }
