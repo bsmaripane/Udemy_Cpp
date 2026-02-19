c390e7ff6fe1ae9ad33306579c91c61a1dd1144a
Belmy Maripane <bsmaripane@gmail.com>
2026-02-19 20:29:56 +0200

    [Test1] - Createa vector and use auto to deduce the type of the iterator
    - Dereference the iterator to display the value
    - Increment and decrement the iterator

--- Project_examples/Section_20_Iterators/Section_20_Iterators/main.cpp
+++ Project_examples/Section_20_Iterators/Section_20_Iterators/main.cpp
@@ -15,3 +15,23 @@ void  display(const std::vector<int>& vec)
 	}
 	std::cout << "]" << std::endl;
 }
+
+void Test1() 
+{
+	std::cout << "\n=====================================================\n";
+	std::vector<int>nums1{ 1,2,3,4,5 };
+	auto it = nums1.begin();	// point to 1
+	std::cout << *it << std::endl;
+
+	it++;	// point to 2
+	std::cout << *it << std::endl;
+
+	it += 2;	// point to 4
+	std::cout << *it << std::endl;
+
+	it -= 2;	// point to 2
+	std::cout << *it << std::endl;
+
+	it = nums1.end() - 1;	// point to 5
+	std::cout << *it << std::endl;
+}
