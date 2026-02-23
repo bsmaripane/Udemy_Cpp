fb97cbc8e21b288159e8b4fac6931fcaa2c28a82
Belmy Maripane <bsmaripane@gmail.com>
2026-02-23 14:56:49 +0200

    [Test1] initialise and assign data to a deque
    - Call display method
    - Add a value to a deque at position 0 and 1

--- Project_examples/Section_20_Deque/Section_20_Deque/main.cpp
+++ Project_examples/Section_20_Deque/Section_20_Deque/main.cpp
@@ -14,8 +14,27 @@ void display(const std::deque<T>& d)
 	std::cout << "]" << std::endl;
 }
 
+void test1()
+{
+	std::cout << "\nTest1 =========================================\n";
+
+	std::deque<int>d{ 1,2,3,4,5 };
+	display(d);
+
+	d = { 2,4,6,8 };
+	display(d);
+
+	std::deque<int> d1(10, 100);	// ten 100s in the deque
+	display(d1);
+
+	d[0] = 100;
+	d.at(1) = 200;
+	display(d);
+}
+
 int main()
 {
+	test1();
 
 	system("pause");
 	return 0;
