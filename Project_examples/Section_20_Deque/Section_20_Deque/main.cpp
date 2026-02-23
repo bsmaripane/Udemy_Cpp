7732fd042d1f7e15ae2cb47ca0af6d8c45769b26
Belmy Maripane <bsmaripane@gmail.com>
2026-02-23 15:33:04 +0200

    [Test4] Puse front vs push back ordering

--- Project_examples/Section_20_Deque/Section_20_Deque/main.cpp
+++ Project_examples/Section_20_Deque/Section_20_Deque/main.cpp
@@ -72,11 +72,31 @@ void Test3()
 	Display(d);
 }
 
+void Test4()
+{
+	std::cout << "\nTest4 =========================================\n";
+
+	std::vector<int> vec{ 0,1,2,3,4,5,6,7,8,9,10 };
+	std::deque<int> d;
+
+	for (const auto& elem : vec)
+		d.push_front(elem);
+
+	Display(d);
+	d.clear();
+
+	for (const auto& elem : vec)
+		d.push_back(elem);
+
+	Display(d);
+}
+
 int main()
 {
 	Test1();
 	Test2();
 	Test3();
+	Test4();
 
 	system("pause");
 	return 0;
