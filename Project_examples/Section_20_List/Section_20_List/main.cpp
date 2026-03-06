0a421b26812429d85b089a9e114ff36598ff73ef
Belmy Maripane <bsmaripane@gmail.com>
2026-03-06 13:37:47 +0200

    feat: add the methods that use iterators

--- Project_examples/Section_20_List/Section_20_List/main.cpp
+++ Project_examples/Section_20_List/Section_20_List/main.cpp
@@ -23,7 +23,19 @@ int main()
 	std::cout << "list1.max_size() : " << list1.max_size() << std::endl;
 
 	std::cout << "list1.front() : " << list1.front() << std::endl;
-	std::cout << "list1.bact() : " << list1.back() << std::endl;
+	std::cout << "list1.back() : " << list1.back() << std::endl;
+
+	// methods that use iterators
+	auto it = std::find(list1.begin(), list1.end(), 4);
+
+	list1.insert(it, 9);
+
+	list1.erase(it);
+	std::cout << "list1.size() : " << list1.size() << std::endl;
+	list1.resize(2);
+	std::cout << "list1.size() : " << list1.size() << std::endl;
+	list1.resize(5);
+	std::cout << "list1.size() : " << list1.size() << std::endl;
 
 	system("pause");
 	return 0;
