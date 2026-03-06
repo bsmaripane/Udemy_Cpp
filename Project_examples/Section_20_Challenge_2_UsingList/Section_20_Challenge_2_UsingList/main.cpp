f98d48a4cad83b0f153df26613da132d0f10ddac
Belmy Maripane <bsmaripane@gmail.com>
2026-03-06 17:53:10 +0200

    feat(Song): add the implementation of the song class

--- Project_examples/Section_20_Challenge_2_UsingList/Section_20_Challenge_2_UsingList/main.cpp
+++ Project_examples/Section_20_Challenge_2_UsingList/Section_20_Challenge_2_UsingList/main.cpp
@@ -7,6 +7,33 @@
 #include <iomanip>
 #include <limits>
 
+class Song
+{
+	friend std::ostream& operator<<(std::ostream& os, const Song& song)
+	{
+		os << std::setw(20) << std::left << song.name
+			<< std::setw(30) << std::left << song.artist
+			<< std::setw(2) << std::left << song.rating;
+
+		return os;
+	}
+	std::string name;
+	std::string artist;
+	int rating;
+public:
+	Song() = default;
+	Song(std::string name, std::string artist, int rating) : name(name), artist(artist) 
+	{
+		if (rating < 0 || rating > 5)
+			this->rating = 0;
+	}
+	std::string GetName() const { return name; }
+	std::string GetArtist() const { return artist; }
+	int GetRating() const { return rating; }
+	bool operator<(const Song& rhs) const { return this->name < rhs.name; }
+	bool operator==(const Song& rhs) const { return this->name == rhs.name; }
+};
+
 int main()
 {
 
