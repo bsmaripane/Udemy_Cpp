531b37ebae0c01ae296952218eae16f512e93e7b
Belmy Maripane <bsmaripane@gmail.com>
2026-03-08 19:29:04 +0200

    feat(display): add template display method to list the map elements

--- Project_examples/Section_20_AssociativeContainers_Maps/Section_20_AssociativeContainers_Maps/main.cpp
+++ Project_examples/Section_20_AssociativeContainers_Maps/Section_20_AssociativeContainers_Maps/main.cpp
@@ -18,6 +18,15 @@ void display(const std::map<std::string, std::set<int>>& aMap)
 	std::cout << "]" << std::endl;
 }
 
+template <typename T1, typename T2>
+void display(const std::map<T1, T2>& aMap)
+{
+	std::cout << "[ ";
+	for (const auto& elem : aMap)
+		std::cout << elem.first << ":" << elem.second << " ";
+	std::cout << "] " << std::endl;
+}
+
 int main()
 {
 
