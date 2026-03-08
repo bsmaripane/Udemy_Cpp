20ff1cd16329eeeafd145fde47fc131842335e20
Belmy Maripane <bsmaripane@gmail.com>
2026-03-08 19:17:30 +0200

    feat(display): add display method to list map elements

--- Project_examples/Section_20_AssociativeContainers_Maps/Section_20_AssociativeContainers_Maps/main.cpp
+++ Project_examples/Section_20_AssociativeContainers_Maps/Section_20_AssociativeContainers_Maps/main.cpp
@@ -5,6 +5,19 @@
 #include <map>
 #include <set>
 
+void display(const std::map<std::string, std::set<int>>& aMap)
+{
+	std::cout << "[ ";
+	for (const auto &elem : aMap)
+	{
+		std::cout << elem.first << ":[";
+		for (const auto& set_elem : elem.second)
+			std::cout << set_elem << " ";
+		std::cout << "] ";
+	}
+	std::cout << "]" << std::endl;
+}
+
 int main()
 {
 
