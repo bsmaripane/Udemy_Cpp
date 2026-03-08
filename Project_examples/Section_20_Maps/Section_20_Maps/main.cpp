eae6b187136805cbdd01851fc8429ac72ff6cff6
Belmy Maripane <bsmaripane@gmail.com>
2026-03-08 18:18:29 +0200

    chore(map): coomon method
    - The size method returns number of elements that are in the map.
    - The max_size return maximum number of elements the map can contain

--- Project_examples/Section_20_Maps/Section_20_Maps/main.cpp
+++ Project_examples/Section_20_Maps/Section_20_Maps/main.cpp
@@ -15,8 +15,16 @@ int main()
 	{
 		{"Felicia", "van der Merwe"},
 		{"Belmy", "Maripane"},
-		{"Lesego", "Maripane"}
+		{"Lesego", "Maripane"},
+		{"Maashele", "Maripane"},
+		{"Kganya", "Maripane"},
+		{"Tracy", "Maripane"}
 	};
+
+	std::cout << "map1.size(): " << map1.size() << std::endl;
+	std::cout << "fullName.size(): " << fullName.size() << std::endl;
+	std::cout << "map1.max_size(): " << map1.max_size() << std::endl;
+	std::cout << "fullName.max_size(): " << fullName.max_size() << std::endl;
 
 	system("pause");
 	return 0;
