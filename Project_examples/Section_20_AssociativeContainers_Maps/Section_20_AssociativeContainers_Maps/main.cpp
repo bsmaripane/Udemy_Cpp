9cadcc2f022d6be0b32c91249797be2068c712aa
Belmy Maripane <bsmaripane@gmail.com>
2026-03-08 21:59:40 +0200

    test(one): insertion and update method of the map
    -  Declare and initialise a map
    - Insert a pair values into the map
    - Insert values to the map
    - Update the age of the map
    - Removes all elements of the map
    - Display list of the map

--- Project_examples/Section_20_AssociativeContainers_Maps/Section_20_AssociativeContainers_Maps/main.cpp
+++ Project_examples/Section_20_AssociativeContainers_Maps/Section_20_AssociativeContainers_Maps/main.cpp
@@ -27,9 +27,40 @@ void display(const std::map<T1, T2>& aMap)
 	std::cout << "] " << std::endl;
 }
 
+void test1()
+{
+	std::cout << "\nTest1 =======================================\n";
+
+	std::map<std::string, int> map
+	{
+		{"Lesego", 13},
+		{"Maxine", 30},
+		{"Semape", 31}
+	};
+	display(map);
+
+	map.insert(std::pair<std::string, int>{"Kganya", 5});
+	display(map);
+
+	map["Maashele"] = 54;
+	display(map);
+
+	map["Maashele"] += 6;
+	display(map);
+
+	map.erase("Semape");
+	display(map);
+
+	std::cout << "Count for Kganya: " << map.count("Kganya") << std::endl;
+	std::cout << "Count for Lesego: " << map.count("Lesego") << std::endl;
+
+	map.clear();
+	display(map);
+}
+
 int main()
 {
-
+	test1();
 
 	system("pause");
 	return 0;
