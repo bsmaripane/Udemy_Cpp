665346237eac599a9e7edd54b014da5e40c3bb1b
Belmy Maripane <bsmaripane@gmail.com>
2026-03-08 14:05:37 +0200

    feat(person): add the implementation of the Person class and the structure of main method

---
+++ Project_examples/Section_20_AssociativeContainers_Sets/Section_20_AssociativeContainers_Sets/main.cpp
@@ -0,0 +1,42 @@
+// Section 20 
+// Associative Containers - Sets
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+
+class Person
+{
+	friend std::ostream &operator<<(std::ostream &os, const Person &person)
+	{
+		os << person.name << " : " << person.age;
+		return os;
+	}
+	std::string name;
+	int age;
+public:
+	Person() : name("Unknown"), age(0) {}
+	Person(std::string name, int age)
+		: name(name)
+	{
+		if (age <= 0)
+			throw std::invalid_argument("Error: age must be greater than 0!\n");
+		else
+			this->age = age;
+	}
+
+	bool operator==(const Person& rhs) const
+	{
+		return (this->name == rhs.name && this->age == rhs.age);
+	}
+};
+
+
+int main()
+{
+
+
+	system("pause");
+	return 0;
+}
