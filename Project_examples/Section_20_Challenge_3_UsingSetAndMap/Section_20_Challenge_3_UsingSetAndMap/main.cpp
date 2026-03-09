5b3b29acefea7641995acd0cf0fd7c946ee7b309
Belmy Maripane <bsmaripane@gmail.com>
2026-03-09 20:37:29 +0200

    style(challenge-3): main method structure

---
+++ Project_examples/Section_20_Challenge_3_UsingSetAndMap/Section_20_Challenge_3_UsingSetAndMap/main.cpp
@@ -0,0 +1,14 @@
+// Section 20
+// Challenge 3 Using std::set and std::map
+
+#include <iostream>
+#include <set>
+#include <map>
+
+int main()
+{
+	std::cout << "Section 20 Challenge 3 Using std::set and std::map\n==================================================\n\n";
+
+	system("pause");
+	return 0;
+}
