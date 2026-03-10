85c8737ff5a39bd31b9f0ee2e36a57b7de69d3d7
Belmy Maripane <bsmaripane@gmail.com>
2026-03-10 19:55:26 +0200

    feat(part-methods): add the surb of aprt 1 and 2 methds and call them in main

--- Project_examples/Section_20_Challenge_3_UsingSetAndMap/Section_20_Challenge_3_UsingSetAndMap/main.cpp
+++ Project_examples/Section_20_Challenge_3_UsingSetAndMap/Section_20_Challenge_3_UsingSetAndMap/main.cpp
@@ -2,12 +2,28 @@
 // Challenge 3 Using std::set and std::map
 
 #include <iostream>
+#include <fstream>
 #include <set>
 #include <map>
+
+void part1()
+{
+
+}
+
+// Part2 process the file and builds a map of words and
+//		 a set of line numbers in which the word appears
+void part2()
+{
+
+}
 
 int main()
 {
 	std::cout << "Section 20 Challenge 3 Using std::set and std::map\n==================================================\n\n";
+
+	part1();
+	part2();
 
 	system("pause");
 	return 0;
