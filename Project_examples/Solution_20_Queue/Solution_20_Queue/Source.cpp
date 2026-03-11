f21f91b711b42f6df83cdd5f9e0d5709889546b4
Belmy Maripane <bsmaripane@gmail.com>
2026-03-11 16:42:17 +0200

    docs(queue); add doc string for display method

--- Project_examples/Solution_20_Queue/Solution_20_Queue/Source.cpp
+++ Project_examples/Solution_20_Queue/Solution_20_Queue/Source.cpp
@@ -6,6 +6,9 @@
 #include <list>
 #include <deque>
 
+// Display the elements of the queue by repeatedly removing elements from the front.
+// The queue is passed in by value, don't affect the passed in queue.
+
 int main()
 {
 	std::cout << "Section 20 Queue\n\n";
