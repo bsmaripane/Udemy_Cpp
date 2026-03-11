4bd8ae44c6f425f89ddb0423694d8957fdfd6677
Belmy Maripane <bsmaripane@gmail.com>
2026-03-11 16:39:26 +0200

    chore(queue): add initial main method stub

---
+++ Project_examples/Solution_20_Queue/Solution_20_Queue/Source.cpp
@@ -0,0 +1,16 @@
+// Section 20 Queue
+
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <list>
+#include <deque>
+
+int main()
+{
+	std::cout << "Section 20 Queue\n\n";
+
+	std::cout << std::endl;
+	system("pause");
+	return 0;
+}
