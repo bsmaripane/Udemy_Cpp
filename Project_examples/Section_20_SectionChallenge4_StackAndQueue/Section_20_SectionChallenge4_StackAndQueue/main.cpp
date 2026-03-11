31df5e57ba41af60589bfae03eaf5e72bbea7b8e
Belmy Maripane <bsmaripane@gmail.com>
2026-03-11 17:25:12 +0200

    chore(stack-queue-challenge): initial main method stub

---
+++ Project_examples/Section_20_SectionChallenge4_StackAndQueue/Section_20_SectionChallenge4_StackAndQueue/main.cpp
@@ -0,0 +1,14 @@
+// Section 20 Section Challenge
+// Stack and Queue challenge
+
+#include <iostream>
+#include <queue>
+
+int main()
+{
+	std::cout << "Section 20: Stack and Queue challenge\n\n";
+
+	std::cout << std::endl;
+	system("pause");
+	return 0;
+}
