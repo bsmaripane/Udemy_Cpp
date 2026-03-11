e8c2273b46f806d4c9d86ebd013dc40b04f3def3
Belmy Maripane <bsmaripane@gmail.com>
2026-03-11 15:25:00 +0200

    dosc(stack): add doc string for display method

--- Project_examples/Solution_20_Stack/Solution_20_Stack/Source.cpp
+++ Project_examples/Solution_20_Stack/Solution_20_Stack/Source.cpp
@@ -5,6 +5,9 @@
 #include <vector>
 #include <list>
 
+// Displays a stack of by repeatedly topping and popping the stack.
+// The stack is passed in value, so don't affaect the passed stack
+
 int main()
 {
 	std::cout << "Section 20 Stack\n\n";
