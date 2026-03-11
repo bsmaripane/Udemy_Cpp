4a621155e551318ee60057cb017edeb75deaf14f
Belmy Maripane <bsmaripane@gmail.com>
2026-03-11 18:40:58 +0200

    feat(priority-queue): implementation of Person class

--- Project_examples/Section_20_Priority_Queue/Section_20_Priority_Queue/Source.cpp
+++ Project_examples/Section_20_Priority_Queue/Section_20_Priority_Queue/Source.cpp
@@ -7,11 +7,30 @@ class Person
 {
 	friend std::ostream& operator<<(std::ostream& os, const Person& person)
 	{
+		os << person.name << ":" << person.age;
 		return os;
 	}
+	std::string name;
+	int age;
 public:
-	Person();
-	Person();
+	Person() : name("Unknown"), age(0) {}
+	Person(std::string name, int age)
+		: name(name)
+	{
+		if (age < 0)
+			std::cerr << "Error: age cannot be a negative integer!" << std::endl;
+		else
+			this->age = age;
+	}
+
+	bool operator<(const Person& rhs) const
+	{
+		return this->age < rhs.age;
+	}
+	bool operator==(const Person& rhs) const
+	{
+		return (this->name == rhs.name && this->age == rhs.age);
+	}
 };
 
 int main()
