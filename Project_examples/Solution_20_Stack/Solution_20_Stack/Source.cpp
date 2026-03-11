42d7f41a56a4f0510d82e2a68ca001add830851e
Belmy Maripane <bsmaripane@gmail.com>
2026-03-11 15:20:21 +0200

    chore(stack): add initial main method stub

---
+++ Project_examples/Solution_20_Stack/Solution_20_Stack/Source.cpp
@@ -0,0 +1,14 @@
+// Section 20 Stack
+
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <list>
+
+int main()
+{
+	std::cout << "Section 20 Stack\n\n";
+
+	system("pause");
+	return 0;
+}
