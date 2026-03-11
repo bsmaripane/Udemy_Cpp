dbcb6678e3e31ace51ca2ee62740f23f58a2610b
Belmy Maripane <bsmaripane@gmail.com>
2026-03-11 17:42:26 +0200

    feat(stack-queue): implement the main method

--- Project_examples/Section_20_SectionChallenge4_StackAndQueue/Section_20_SectionChallenge4_StackAndQueue/main.cpp
+++ Project_examples/Section_20_SectionChallenge4_StackAndQueue/Section_20_SectionChallenge4_StackAndQueue/main.cpp
@@ -18,6 +18,15 @@ int main()
 {
 	std::cout << "Section 20: Stack and Queue challenge\n\n";
 
+	std::vector<std::string> test_strings{ "a", "aa", "Aa", "aba", "aBBa", "abbcbba", "ab", "abc", "radar", "bob",
+		"ana", "avid diva", "Amore, Roma", "A Toyata's a toyota", "A Santa at NASA", "C++",
+		"A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a panlindromw", "palindrome" };
+
+	std::cout << std::boolalpha;
+	std::cout << std::setw(8) << std::left << "Result" << "String\n====================================\n";
+	for (size_t i{ 0 }; i < test_strings.size(); i++)
+		std::cout << std::setw(8) << std::left << is_palindrome(test_strings.at(i)) << test_strings.at(i) << std::endl;
+
 	std::cout << std::endl;
 	system("pause");
 	return 0;
