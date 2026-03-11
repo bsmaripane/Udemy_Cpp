04174ea9c64317e9366c72c24eec60eca336e13e
Belmy Maripane <bsmaripane@gmail.com>
2026-03-11 16:00:52 +0200

    feat(stack): add push example using for loop
    - For loop to push elements into a stack
    - Call display method to print element to the console

--- Project_examples/Solution_20_Stack/Solution_20_Stack/Source.cpp
+++ Project_examples/Solution_20_Stack/Solution_20_Stack/Source.cpp
@@ -23,6 +23,15 @@ void display(std::stack<Type> stack)
 int main()
 {
 	std::cout << "Section 20 Stack\n\n";
+	
+	std::stack<int> stackNums;
+	std::stack<int, std::vector<int>> stackVector;
+	std::stack<int, std::list<int>> stackList;
+	std::stack<int, std::deque<int>> stackDeque;
+
+	for (int item : { 1, 2, 3, 4, 5})
+		stackNums.push(item);
+	display(stackNums);
 
 	system("pause");
 	return 0;
