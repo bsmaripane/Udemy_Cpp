4c09371e66de99e52473f0db020e8e67e8f34c1c
Belmy Maripane <bsmaripane@gmail.com>
2026-03-17 21:22:53 +0200

    fix(CalculatorTest): missing precompiled header

--- AutomatedTest/CalculatorTests/CalculatorTests/test.cpp
+++ AutomatedTest/CalculatorTests/CalculatorTests/test.cpp
@@ -1,8 +1,10 @@
+#include "pch.h"
+#include "CppUnitTest.h"
 #include <gtest/gtest.h>
 #include "Calculator.h"
 
 TEST(CalculatorTest, AddTwoNumber) 
 {
 	Calculator calc;
-	EXPECT_EQ(1, 1);
+	EXPECT_EQ(calc.add(4, 12), 16);
 }
