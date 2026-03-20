373596bc99c8a56c37b3b95102511fed447a6d16
Belmy Maripane <bsmaripane@gmail.com>
2026-03-20 14:01:40 +0200

    feat(StudentDatabaseTDD): minal implementation of StudentDatabase class

--- AutomatedTest/StudentDatabaseTDD/StudentDatabaseTDD/StudentDatabase.cpp
+++ AutomatedTest/StudentDatabaseTDD/StudentDatabaseTDD/StudentDatabase.cpp
@@ -1,1 +1,11 @@
 #include "StudentDatabase.h"
+
+void StudentDatabase::addStudent(int studentId, std::string studentName)
+{
+	count++;
+}
+
+int StudentDatabase::getCount() const
+{
+	return count;
+}
--- AutomatedTest/StudentDatabaseTDD/StudentDatabaseTDD/StudentDatabase.h
+++ AutomatedTest/StudentDatabaseTDD/StudentDatabaseTDD/StudentDatabase.h
@@ -1,5 +1,12 @@
 #pragma once
+#include <string>
+
 class StudentDatabase
 {
+public:
+	void addStudent(int studentId, std::string studentName);
+	int getCount() const;
+private:
+	int count = 0;
 };
 
