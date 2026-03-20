186ee1ef3d77c1d84e3864694a3b6868b6fa22ad
Belmy Maripane <bsmaripane@gmail.com>
2026-03-20 14:50:23 +0200

    refact(StudentDatabaseTDD): use google test fixture
    - Avoids duplicating db object in every tests
    - Makes tests cleaner and DRY (Don't Repeat Yourself)

--- AutomatedTest/StudentDatabaseTDD/StudentDatabaseTDD/StudentDatabaseTests.cpp
+++ AutomatedTest/StudentDatabaseTDD/StudentDatabaseTDD/StudentDatabaseTests.cpp
@@ -1,22 +1,25 @@
 #include "gtest/gtest.h"
 #include "StudentDatabase.h"
 
-TEST(StudentDatabaseTest, AddStudent_ShouldIncreaseCount)
+class StudentDatabaseTest : public ::testing::Test
 {
+protected:
 	StudentDatabase db;
+};
+
+TEST_F(StudentDatabaseTest, AddStudent_ShouldIncreaseCount)
+{
 	db.addStudent(101, "Maxine");
 	EXPECT_EQ(db.getCount(), 1);
 }
 
-TEST(StudentDatabase, GetStudentById_ShouldReturnCorrectName)
+TEST_F(StudentDatabaseTest, GetStudentById_ShouldReturnCorrectName)
 {
-	StudentDatabase db;
 	db.addStudent(101, "Maxine");
 	EXPECT_EQ(db.getStudentName(101), "Maxine");
 }
 
-TEST(StudentDatabase, GetStudent_InvalidId_ShouldReturnEmpty)
+TEST_F(StudentDatabaseTest, GetStudent_InvalidId_ShouldReturnEmpty)
 {
-	StudentDatabase db;
 	EXPECT_EQ(db.getStudentName(-1), "");
 }
